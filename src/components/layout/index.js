478ca6780b631b3423fc50534e034a55832bcb30
zahra <zahra20hb@gmail.com>
2023-12-12 02:10:18 -0800

    some problem fixed

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -4,12 +4,10 @@ import React from "react";
 const Layout = ({ children }) => {
   return (
     <>
-      <header className="w-full h-[100px] flex justify-center items-center">
-        Header
-      </header>
-      <div className="w-full flex">
-        <div className="flex-1 h-[500px]">{children}</div>
-        <aside className="w-[200px] h-full">
+
+      <div className="flex">
+
+        <aside className="w-[200px] basis-[15%] h-full">
           <div>
             <Link href="/">Home</Link>
           </div>
@@ -17,10 +15,24 @@ const Layout = ({ children }) => {
             <Link href="/about">ABOUT</Link>
           </div>
         </aside>
+
+
+        <div className="w-full flex flex-col">
+
+          <header className="w-full h-[100px] flex justify-center items-center">
+            Header
+          </header>
+
+          <div className="flex-1 h-[500px]">{children}</div>
+
+          <footer className="w-full h-[100px] flex justify-center items-center">
+            footer
+          </footer>
+
+        </div>
+
       </div>
-      <footer className="w-full h-[100px] flex justify-center items-center">
-        footer
-      </footer>
+
     </>
   );
 };
--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -3,8 +3,8 @@ export default function ABOUT() {
   return (
     <main className="flex justify-center items-center w-full h-full border-lime-500 border-2">
       ABOUT
-      <div className={styles.a}></div>
-      <div className={styles.b}></div>
+      {/* <div className={styles.a}></div>
+      <div className={styles.b}></div> */}
     </main>
   );
 }
