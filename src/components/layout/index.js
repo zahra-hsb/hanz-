87926a14d75b00d4d29e45a18985d69c0f2cfc9d
zahra <zahra20hb@gmail.com>
2023-12-12 02:38:07 -0800

    sidebar alignments created

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -7,12 +7,38 @@ const Layout = ({ children }) => {
 
       <div className="flex">
 
-        <aside className="w-[200px] basis-[15%] h-full">
-          <div>
+        <aside className="flex flex-col justify-between basis-[15%] w-[200px] h-[90vh]">
+          {/* <div>
             <Link href="/">Home</Link>
           </div>
           <div>
             <Link href="/about">ABOUT</Link>
+          </div> */}
+          <div>
+            <div className="">1</div>
+            <div className="flex">
+              <div className="basis-[50%]">21</div>
+              <div className="basis-[50%]">22</div>
+            </div>
+            <div className="">
+              <div>31</div>
+              <div>32</div>
+              <div>33</div>
+              <div>34</div>
+              <div>35</div>
+            </div>
+          </div>
+          <div>
+            <div className="flex">
+              <div>41</div>
+              <div>42</div>
+              <div>43</div>
+              <div>44</div>
+            </div>
+            <div className="flex">
+              <div className="basis-[50%]">51</div>
+              <div className="basis-[50%]">52</div>
+            </div>
           </div>
         </aside>
 
