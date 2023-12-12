1d747cf172001339cb578958a1bb3ca935ec44c9
zahra <zahra20hb@gmail.com>
2023-12-12 02:51:28 -0800

    Sidebar component created

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+import Sidebar from "../Sidebar";
 
 const Layout = ({ children }) => {
   return (
@@ -7,40 +8,7 @@ const Layout = ({ children }) => {
 
       <div className="flex">
 
-        <aside className="hidden lg:flex flex-col justify-between basis-[15%] w-[200px] h-[90vh]">
-          {/* <div>
-            <Link href="/">Home</Link>
-          </div>
-          <div>
-            <Link href="/about">ABOUT</Link>
-          </div> */}
-          <div>
-            <div className="">1</div>
-            <div className="flex">
-              <div className="basis-[50%]">21</div>
-              <div className="basis-[50%]">22</div>
-            </div>
-            <div className="">
-              <div>31</div>
-              <div>32</div>
-              <div>33</div>
-              <div>34</div>
-              <div>35</div>
-            </div>
-          </div>
-          <div>
-            <div className="flex">
-              <div>41</div>
-              <div>42</div>
-              <div>43</div>
-              <div>44</div>
-            </div>
-            <div className="flex">
-              <div className="basis-[50%]">51</div>
-              <div className="basis-[50%]">52</div>
-            </div>
-          </div>
-        </aside>
+        <Sidebar />
 
 
         <div className="w-full flex flex-col">
