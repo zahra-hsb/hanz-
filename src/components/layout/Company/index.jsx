ec6189b910291dc0380af19b106a50385c556209
zahra <zahra20hb@gmail.com>
2023-12-16 23:23:37 +0330

    company section component created and alignments added

---
+++ src/components/layout/Company/index.jsx
@@ -0,0 +1,23 @@
+const Company = () => {
+    return (
+        <>
+            <section className="flex flex-col w-full p-3">
+                <div className="flex items-center justify-between px-5">
+                    <div>1</div>
+                    <div>2</div>
+                </div>
+                <div>2</div>
+            </section>
+            <section className="flex w-full items-center justify-around flex-wrap p-3">
+                <div className="h-full">wwwwwwwww</div>
+                <div className="p-2">
+                    <div>11</div>
+                    <div>22</div>
+                    <div>33</div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Company
