0a4dc0c7875be6cc13e87da1cbb1cb6516da86e8
zahra <zahra20hb@gmail.com>
2023-12-16 23:57:20 +0330

    slider component created

---
+++ src/components/layout/Slider/index.jsx
@@ -0,0 +1,32 @@
+const Slider = () => {
+    return (
+        <>
+            <section className="flex flex-col w-full p-5">
+                <div className="flex items-center justify-between p-3">
+                    <div>1</div>
+                    <div>2</div>
+                </div>
+                <div className="p-2">
+                    <span>2</span>
+                    <span>2</span>
+                </div>
+                <div className="flex items-center justify-center">
+                    <div>
+                        <div>2</div>
+                        <div className="p-3">2</div>
+                    </div>
+                    <div>
+                        <div>2</div>
+                        <div className="p-3">2</div>
+                    </div>
+                    <div>
+                        <div>2</div>
+                       <div className="p-3">2</div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Slider
