f801b7f673f2a3dcc15666335aced4bf9799152c
zahra <zahra20hb@gmail.com>
2023-12-16 21:26:05 +0330

    category section elements added and some problem fixed

--- src/components/layout/CategorySection/index.jsx
+++ src/components/layout/CategorySection/index.jsx
@@ -1,7 +1,15 @@
 const CategorySection = () => {
     return (
         <>
-            
+            <section>
+                <div>1</div>
+                <div>
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                    <div>4</div>
+                </div>
+            </section>
         </>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import Main from "@/components/layout/Main";
 
 export default function Home() {
   return (
-    <main className="lg:w-[85%] flex justify-center items-center w-full h-full border-lime-500 border-2">
+    <main className="lg:w-full flex justify-center items-center w-full h-full border-lime-500 border-2">
       <Main />
     </main>
   );
