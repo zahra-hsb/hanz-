1247936e4bfb44eeb51725f0362d54eba5c10ac3
zahra <zahra20hb@gmail.com>
2023-12-16 22:16:18 +0330

    alignments of about section components created

--- src/components/layout/AboutSection/index.jsx
+++ src/components/layout/AboutSection/index.jsx
@@ -1,7 +1,29 @@
 const AboutSection = () => {
     return (
         <>
-            
+            <section>
+                <div>
+                    <div>1</div>
+                    <div>
+                        <div>1</div>
+                        <div>2</div>
+                        <div>3</div>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <div>1</div>
+                        <div>2</div>
+                    </div>
+                </div>
+                <div>
+                    <div>1</div>
+                    <div>
+                        <div>1</div>
+                        <div>2</div>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import AboutSection from "@/components/layout/AboutSection";
 import CategorySection from "@/components/layout/CategorySection";
 import Main from "@/components/layout/Main";
 
@@ -6,7 +7,7 @@ export default function Home() {
     <main className="lg:w-full flex flex-col justify-center items-center w-full h-full border-lime-500 border-2">
       <Main />
       <CategorySection />
-
+      <AboutSection />
     </main>
   );
 }
