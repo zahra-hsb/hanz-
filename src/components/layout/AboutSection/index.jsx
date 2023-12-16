5da60ffc3059eaeca8a94b4fa08da173c567f3d4
zahra <zahra20hb@gmail.com>
2023-12-16 22:58:16 +0330

    alignments of about section components created

--- src/components/layout/AboutSection/index.jsx
+++ src/components/layout/AboutSection/index.jsx
@@ -1,26 +1,24 @@
 const AboutSection = () => {
     return (
         <>
-            <section>
-                <div>
-                    <div>1</div>
-                    <div>
-                        <div>1</div>
-                        <div>2</div>
-                        <div>3</div>
+            <section className="flex flex-col p-1 w-full h-[60vh]">
+                <div className="flex w-full items-center justify-around flex-wrap">
+                    <div className="h-full">1</div>
+                    <div className="p-2">
+                        <div>11</div>
+                        <div>22</div>
+                        <div>33</div>
                     </div>
                 </div>
-                <div>
-                    <div>
-                        <div>1</div>
-                        <div>2</div>
-                    </div>
+                <div className="w-1/2 p-5">
+                    <div>a</div>
+                    <div>b</div>
                 </div>
-                <div>
-                    <div>1</div>
-                    <div>
-                        <div>1</div>
-                        <div>2</div>
+                <div className="flex w-full items-center justify-around flex-wrap">
+                    <div className="h-full">1111111111111</div>
+                    <div className="p-2">
+                        <div>212222222222</div>
+                        <div>223333333333</div>
                     </div>
                 </div>
             </section>
