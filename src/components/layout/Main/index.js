00e5497249a7645bd21d390452ed56ca599259be
zahra <zahra20hb@gmail.com>
2023-12-16 15:35:51 +0330

    inner spaces for elements added

--- src/components/layout/Main/index.js
+++ src/components/layout/Main/index.js
@@ -1,11 +1,11 @@
 const Main = () => {
     return (
         <>
-            <main className="w-full h-[400px] relative">
+            <main className="w-full h-[350px] relative p-8">
                 <div className="absolute top-0 left-0 bg-black opacity-10 w-full h-full"></div>
                 <div className="flex flex-col">
-                    <div>1</div>
-                    <div>2</div>
+                    <div className="p-2">1</div>
+                    <div className="p-1">2</div>
                 </div>
             </main>
         </>
