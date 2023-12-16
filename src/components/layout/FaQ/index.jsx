189f5c7c8301334d0dd9bff7ef45f8876449398c
zahra <zahra20hb@gmail.com>
2023-12-16 23:36:07 +0330

    alignments of faq component added

--- src/components/layout/FaQ/index.jsx
+++ src/components/layout/FaQ/index.jsx
@@ -1,7 +1,21 @@
 const FaQ = () => {
     return (
         <>
-            
+            <section className="flex flex-col w-full items-center p-5">
+                <div>1</div>
+                <div>2</div>
+                <div className="flex flex-wrap items-center justify-center w-full p-4">
+                    <div className="p-5">11111</div>
+                    <div className="p-5">11111</div>
+                    <div className="p-5">11111</div>
+                    <div className="p-5">11111</div>
+                    <div className="p-5">11111</div>
+                    <div className="p-5">11111</div>
+                    <div className="p-5">11111</div>
+                    <div className="p-5">11111</div>
+                </div>
+                <div>4</div>
+            </section>
         </>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import AboutSection from "@/components/layout/AboutSection";
 import CategorySection from "@/components/layout/CategorySection";
 import Company from "@/components/layout/Company";
+import FaQ from "@/components/layout/FaQ";
 import Main from "@/components/layout/Main";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <CategorySection />
       <AboutSection />
       <Company />
+      <FaQ />
     </main>
   );
 }
