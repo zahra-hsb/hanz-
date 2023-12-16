498f93c76b1956fab712c33b499079f5fdc5f9ea
zahra <zahra20hb@gmail.com>
2023-12-16 01:04:50 -0800

    main component styles added

--- src/components/layout/Main/index.js
+++ src/components/layout/Main/index.js
@@ -1,7 +1,13 @@
 const Main = () => {
     return (
         <>
-            
+            <main className="w-full h-[400px] relative">
+                <div className="absolute top-0 left-0 bg-black opacity-10 w-full h-full"></div>
+                <div className="flex flex-col">
+                    <div>1</div>
+                    <div>2</div>
+                </div>
+            </main>
         </>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
+import Main from "@/components/layout/Main";
+
 export default function Home() {
   return (
-    <main className="flex justify-center items-center w-full h-full border-lime-500 border-2">
-      HOME
+    <main className="lg:w-[85%] flex justify-center items-center w-full h-full border-lime-500 border-2">
+      <Main />
     </main>
   );
 }
