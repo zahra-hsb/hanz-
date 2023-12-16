04f862775c5344f21d07afff805b01293c579bb6
zahra <zahra20hb@gmail.com>
2023-12-16 21:59:37 +0330

    category section styles added

--- src/components/layout/CategorySection/index.jsx
+++ src/components/layout/CategorySection/index.jsx
@@ -1,13 +1,13 @@
 const CategorySection = () => {
     return (
         <>
-            <section>
+            <section className="flex flex-col w-full items-center">
                 <div>1</div>
-                <div>
-                    <div>1</div>
-                    <div>2</div>
-                    <div>3</div>
-                    <div>4</div>
+                <div className="flex flex-row w-full items-center justify-around flex-wrap">
+                    <div>11111111111111</div>
+                    <div>22222222222222</div>
+                    <div>33333333333333</div>
+                    <div>44444444444444</div>
                 </div>
             </section>
         </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
+import CategorySection from "@/components/layout/CategorySection";
 import Main from "@/components/layout/Main";
 
 export default function Home() {
   return (
-    <main className="lg:w-full flex justify-center items-center w-full h-full border-lime-500 border-2">
+    <main className="lg:w-full flex flex-col justify-center items-center w-full h-full border-lime-500 border-2">
       <Main />
+      <CategorySection />
+
     </main>
   );
 }
