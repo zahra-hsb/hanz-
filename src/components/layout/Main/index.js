e9d0dbe51f3194eb0de0ed87d69c1e0970754bf2
zahra <zahra20hb@gmail.com>
2023-12-18 23:11:37 +0330

    spaces between category section

--- src/components/layout/Main/index.js
+++ src/components/layout/Main/index.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 const Main = () => {
     return (
         <>
-            <main className="w-full h-[350px] relative p-8 lg:pr-20 bg-mainSection bg-center text-white">
+            <main className="w-full h-[500px] relative p-20 lg:pr-20 bg-mainSection bg-center text-white">
                 <div className="flex flex-col">
                     <div className="p-4 mt-5">
                         <h3 className="absolute z-50 text-3xl py-4 px-2 ml-4 border-y-4 border-r-4 border-green">
