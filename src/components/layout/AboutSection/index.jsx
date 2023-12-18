870fc7d69045599abecc9bb0808d504953d66c3f
zahra <zahra20hb@gmail.com>
2023-12-18 20:33:07 +0330

    section two of aboutsection component added

--- src/components/layout/AboutSection/index.jsx
+++ src/components/layout/AboutSection/index.jsx
@@ -29,9 +29,20 @@ const AboutSection = () => {
                         </div>
                     </div>
                 </div>
-                <div className="w-1/2 p-5">
-                    <div>a</div>
-                    <div>b</div>
+                <div className="w-1/2 p-5 m-10">
+                    <div>
+                        <h4 className="text-base font-bold">
+                            واحد طراحی سیستم <span className="opacity-50">(SYSTEM DESIGN LINE):</span>
+                        </h4>
+                    </div>
+                    <div>
+                        <p className="py-5 w-5/6 text-justify opacity-50 text-base">
+                        شرکت شایکو افتخار دارد در این واحد به کمک مهندسین مجرب و جوان و کار آزموده
+                         و با تکیه بر بررسی نیازهای بازار اقدام به طراحی و ساخت سیستم 
+                        های مدرن و نوین در بخش صنعت ساختمان به خصوص نماهای مدرن و نوین ساختمانی
+                         جهت پاسخگویی به انواع سلیقه های مشتریان و معماران عزیز می نماید.
+                        </p>
+                    </div>
                 </div>
                 <div className="flex w-full items-center justify-around flex-wrap">
                     <div className="h-full">1111111111111</div>
