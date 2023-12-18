e2305ea2c1b13820707d0c466e9697424f209777
zahra <zahra20hb@gmail.com>
2023-12-18 13:25:44 +0330

    main section styles and elements added

--- src/components/layout/Main/index.js
+++ src/components/layout/Main/index.js
@@ -1,12 +1,21 @@
+import Link from 'next/link'
 const Main = () => {
     return (
         <>
-            <main className="w-full h-[350px] relative p-8 bg-mainSection bg-center">
-                <div className="absolute top-0 left-0 bg-black opacity-10 w-full h-full"></div>
+            <main className="w-full h-[350px] relative p-8 bg-mainSection bg-center text-white">
                 <div className="flex flex-col">
-                    <div className="p-2">1</div>
-                    <div className="p-1">2</div>
+                    <div className="p-4 mt-5">
+                        <h3 className="absolute z-50 text-3xl py-4 px-2 border-y-4 border-r-4 border-green">
+                            سیستم های نوین درب و پنجره
+                        </h3>
+                    </div>
+                    <div className="p-1">
+                        <Link href={'/products'} className="absolute z-50 mt-20 mr-3 px-2">
+                        مشاهده بیشتر ← 
+                        </Link>
+                    </div>
                 </div>
+                <div className="absolute top-0 left-0 bg-black opacity-20 w-full h-full"></div>
             </main>
         </>
     )
