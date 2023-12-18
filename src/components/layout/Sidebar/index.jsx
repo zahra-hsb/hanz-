e8a776296f5a23b0d830356b3b42ded94d7c51ff
zahra <zahra20hb@gmail.com>
2023-12-18 00:05:42 -0800

    edit menu icon in header component

--- src/components/layout/Sidebar/index.jsx
+++ src/components/layout/Sidebar/index.jsx
@@ -5,14 +5,7 @@ const Sidebar = () => {
     // console.log(show);
     return (
         <>
-            <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg lg:hidden">
-                {/* <span className="sr-only">Open sidebar</span> */}
-                <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
-                </svg>
-            </button>
-
-            <aside id="default-sidebar" className="hidden lg:flex flex-col justify-between w-[100%] lg:w-[15%] h-[100vh] fixed top-0 right-0 z-40 transition-transform -translate-x-full sm:translate-x-0 border-l-[1px]" aria-label="Sidebar">
+            <aside id="default-sidebar" className="hidden lg:flex flex-col justify-between w-[100%] lg:w-[240px] h-[100vh] fixed top-0 right-0 z-40 transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
                 <div>
                     <div className="flex justify-center pt-4">
                         <Image src={hanzIcon} alt="#" />
@@ -38,7 +31,7 @@ const Sidebar = () => {
                     </div>
                     <div className="">
 
-                        {/* <menu id="default-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar"> */}
+                        <menu id="default-sidebar" className="transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
                         <div className="h-full px-3 py-4 overflow-y-auto dark:bg-gray-800">
                             <ul className="space-y-2 font-medium">
                                 <li>
@@ -49,13 +42,11 @@ const Sidebar = () => {
                                 <li>
                                     <Link href="/products" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                                         <span className="flex-1 ms-3 whitespace-nowrap">معرفی محصول</span>
-                                        {/* <span className="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300">Pro</span> */}
                                     </Link>
                                 </li>
                                 <li>
                                     <Link href="/galleryImage" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                                         <span className="flex-1 ms-3 whitespace-nowrap">گالری تصاویر</span>
-                                        {/* <span className="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">3</span> */}
                                     </Link>
                                 </li>
                                 <li>
@@ -68,19 +59,9 @@ const Sidebar = () => {
                                         <span className="flex-1 ms-3 whitespace-nowrap">تماس با ما</span>
                                     </Link>
                                 </li>
-                                {/* <li>
-                                        <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                                            <span className="flex-1 ms-3 whitespace-nowrap">Sign In</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                                            <span className="flex-1 ms-3 whitespace-nowrap">Sign Up</span>
-                                        </a>
-                                    </li> */}
                             </ul>
                         </div>
-                        {/* </menu> */}
+                        </menu>
                     </div>
                 </div>
                 <div>
