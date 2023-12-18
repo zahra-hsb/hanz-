168ce425676e57d969605d3db788f2bb654e9aab
zahra <zahra20hb@gmail.com>
2023-12-17 23:54:35 -0800

    created callUs component and link in sidbar

--- src/components/layout/Sidebar/index.jsx
+++ src/components/layout/Sidebar/index.jsx
@@ -53,7 +53,7 @@ const Sidebar = () => {
                                     </Link>
                                 </li>
                                 <li>
-                                    <Link href="/" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                    <Link href="/galleryImage" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                                         <span className="flex-1 ms-3 whitespace-nowrap">گالری تصاویر</span>
                                         {/* <span className="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">3</span> */}
                                     </Link>
@@ -64,7 +64,7 @@ const Sidebar = () => {
                                     </Link>
                                 </li>
                                 <li>
-                                    <Link href="/" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                    <Link href="/callUs" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                                         <span className="flex-1 ms-3 whitespace-nowrap">تماس با ما</span>
                                     </Link>
                                 </li>
