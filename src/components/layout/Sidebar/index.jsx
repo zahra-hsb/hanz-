836eca3601db6c913eede6f8ab19b26bd4568e50
zahra <zahra20hb@gmail.com>
2023-12-17 23:50:56 -0800

    created [200~import styles from ./products.module.css; component

--- src/components/layout/Sidebar/index.jsx
+++ src/components/layout/Sidebar/index.jsx
@@ -47,7 +47,7 @@ const Sidebar = () => {
                                     </Link>
                                 </li>
                                 <li>
-                                    <Link href="/" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                    <Link href="/products" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                                         <span className="flex-1 ms-3 whitespace-nowrap">معرفی محصول</span>
                                         {/* <span className="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300">Pro</span> */}
                                     </Link>
