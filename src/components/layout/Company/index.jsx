e5299a6b22a20b652022b7eeb8c815aafaa8dd5b
zahra <zahra20hb@gmail.com>
2023-12-19 14:33:05 +0330

    text and button created

--- src/components/layout/Company/index.jsx
+++ src/components/layout/Company/index.jsx
@@ -1,12 +1,22 @@
+import Button from "../Main/button"
+
 const Company = () => {
     return (
         <>
             <section className="flex flex-col w-full p-3">
                 <div className="flex items-center justify-between px-5">
-                    <div>1</div>
-                    <div>2</div>
+                    <div>
+                        <h3 className="text-2xl font-bold">
+                            گروه صنعتی هانز
+                        </h3>
+                    </div>
+                    <div>
+                            <Button url={'/about'} title={'درباره ما'}/>
+                    </div>
                 </div>
-                <div>2</div>
+                <div>
+                    
+                </div>
             </section>
             <section className="flex w-full items-center justify-around flex-wrap p-3">
                 <div className="h-full">wwwwwwwww</div>
