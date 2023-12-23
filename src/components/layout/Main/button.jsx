98531b0b953cd2c41f39b6f0d4cdd4146f954a0c
zahra <zahra20hb@gmail.com>
2023-12-22 23:49:25 -0800

    texts size and spaces created

--- src/components/layout/Main/button.jsx
+++ src/components/layout/Main/button.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const Button = ({ title, url, border, color }) => {
     return (
         <>
-            <Link href={url} className={`z-50 text-green text-lg px-4 py-1 mt-[100px] rounded-3xl ${border} ${color}`}>{title} ←</Link>
+            <Link href={url} className={`z-50 text-green text-md px-4 py-1 mt-[100px] rounded-3xl ${border} ${color}`}>{title} ←</Link>
         </>
     )
 }
--- src/components/layout/Main/index.js
+++ src/components/layout/Main/index.js
@@ -6,12 +6,12 @@ const Main = () => {
             <main className="w-full h-[500px] relative lg:p-20 px-[5%] pt-16 lg:pr-20 bg-mainSection bg-center text-white">
                 <div className="flex flex-col">
                     <div className="lg:p-4 p-2 mt-5">
-                        <h3 className="absolute z-10 lg:text-4xl text-3xl py-4 px-2 ml-4 border-y-4 border-r-4 border-green">
+                        <h3 className="absolute z-10 lg:text-4xl sm:text-2xl text-xl py-4 px-2 ml-4 border-y-4 border-r-4 border-green">
                             سیستم های نوین درب و پنجره
                         </h3>
                     </div>
-                    <div className="p-1 absolute z-10 mt-[30vh] lg:mt-32 mr-3 px-2 w-[200px]">
-                        <Button title={' مشاهده بیشتر'} url={'/products'} color={'text-white'}/>
+                    <div className="p-1 absolute z-10 mt-[150px] mr-3 px-2 w-[200px]">
+                        <Button title={' مشاهده بیشتر'} url={'/products'} color={'text-white text-sm lg:text-lg'}/>
                     </div>
                 </div>
                 <div className="absolute top-0 left-0 bg-black opacity-20 w-full h-full"></div>
