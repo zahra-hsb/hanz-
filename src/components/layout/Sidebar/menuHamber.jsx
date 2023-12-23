f62cd23af000e0bd564da4592352556bc7c85117
zahra <zahra20hb@gmail.com>
2023-12-22 22:15:47 -0800

    overflow hidden for hamburger menu

--- src/components/layout/Sidebar/menuHamber.jsx
+++ src/components/layout/Sidebar/menuHamber.jsx
@@ -1,12 +1,17 @@
 import SidebarComponents from "./utils-sidebar/SidebarComponents";
+import { toggleOverflow } from "@/components/utils";
 
 const MenuHamber = ({ setSidebarShow }) => {
+
 
     return (
         <>
             <div
-                onClick={() => setSidebarShow((s) => !s)}
-                className="bg-black bg-opacity-25 fixed top-0 right-0 z-10 w-full h-screen">
+                onClick={() => {
+                    setSidebarShow((s) => !s)
+                    toggleOverflow()
+                }}
+                className="bg-black bg-opacity-25 fixed top-0 right-0 z-20 w-full h-screen">
 
             </div>
             <SidebarComponents />
