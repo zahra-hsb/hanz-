ebb7e8f8ec98036a0cc2dae0ef97bfaa1b74fecb
mohamad sd <mohamadsedighbakhshaish@gmail.com>
2023-12-25 23:28:19 -0800

    edit text in home banner

--- src/components/layout/Main/index.js
+++ src/components/layout/Main/index.js
@@ -2,15 +2,18 @@ import Button from './button'
 const Main = () => {
     return (
         <>
-            <main className="w-full lg:h-[650px] h-[500px] relative sm:p-20 px-[5%] pt-[100px] mt-[45px] lg:pr-20 bg-mainSection bg-center bg-cover text-white">
+            <main className="w-full lg:h-[650px] h-[500px] relative sm:p-20 px-[5%] pt-[50px] mt-[45px] lg:pr-20 bg-mainSection bg-center bg-cover text-white">
                 <div className="flex flex-col">
                     <div className="lg:p-4 p-2 mt-5">
-                        <h3 className="absolute z-10 lg:text-4xl sm:text-2xl text-xl py-4 px-2 ml-4 mt-5 border-y-4 border-r-4 border-green">
-                            سیستم های نوین درب و پنجره
-                        </h3>
+                        <div className="absolute z-10 py-4 px-2 ml-4 mt-5 border-y-4 border-r-4 border-green">
+                            <h3 className="lg:text-4xl sm:text-2xl text-xl">
+                                سیستم های نوین درب و پنجره
+                            </h3>
+                            <p className='pt-4'>ما با هم ساختمان های پایدار با راه حل های<br /> آلومینیومی با کیفیت بالا ایجاد می کنیم.</p>
+                        </div>
                     </div>
-                    <div className="p-1 absolute z-10 mt-[150px] mr-3 px-2 w-[200px]">
-                        <Button title={' مشاهده بیشتر'} url={'/products'} color={'text-white text-sm lg:text-lg'}/>
+                    <div className="p-1 absolute z-10 mt-[220px] mr-3 px-2 w-[200px]">
+                        <Button title={' مشاهده بیشتر'} url={'/products'} color={'text-white text-sm lg:text-lg'} />
                     </div>
                 </div>
                 <div className="absolute top-0 left-0 bg-black opacity-20 w-full h-full"></div>
