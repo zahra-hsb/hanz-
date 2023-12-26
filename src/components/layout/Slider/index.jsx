126d47e2f4bd197687b3b8d664ec37a59df16ea1
mohamad sd <mohamadsedighbakhshaish@gmail.com>
2023-12-26 02:08:29 -0800

    created project slider

--- src/components/layout/Slider/index.jsx
+++ src/components/layout/Slider/index.jsx
@@ -1,10 +1,11 @@
 import Button from "../Main/button"
+import BigSlider from "./BigSlider"
 
 const Slider = () => {
     return (
         <>
-            <section className="flex flex-col w-full p-5">
-                <div className="flex items-center justify-between p-3">
+            <section className="flex flex-col w-full p-5 mb-36 mt-16">
+                <div className="flex items-center justify-between p-3 my-4">
                     <div>
                         <h3>پروژه های انجام شده</h3>
                     </div>
@@ -13,7 +14,8 @@ const Slider = () => {
 
                     </div>
                 </div>
-                <div className="p-2">
+                <BigSlider slidesPerView={3} textSlider={true} textSlider1={'چرخه زندگی آلومینیوم'} textSlider2={'عایق صوتی: صدای ناخواسته داخل خانه را کاهش دهید'} textSlider3={'عایق صوتی: صدای ناخواسته داخل خانه را کاهش دهید'} textSlider4={'چرخه زندگی آلومینیوم'} sliderCount={''} />
+                {/* <div className="p-2">
                     <span>2</span>
                     <span>2</span>
                 </div>
@@ -30,7 +32,7 @@ const Slider = () => {
                         <div>2</div>
                         <div className="p-3">2</div>
                     </div>
-                </div>
+                </div> */}
             </section>
         </>
     )
