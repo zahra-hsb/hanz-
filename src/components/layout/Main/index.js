e7ef368954302f71a91f967560e1044dcfd59163
mohamad sd <mohamadsedighbakhshaish@gmail.com>
2023-12-25 22:59:25 -0800

    edit size home banner

--- src/components/layout/Main/index.js
+++ src/components/layout/Main/index.js
@@ -2,7 +2,7 @@ import Button from './button'
 const Main = () => {
     return (
         <>
-            <main className="w-full h-[750px] relative sm:p-20 px-[5%] pt-[100px] mt-[45px] lg:pr-20 bg-mainSection bg-center bg-cover text-white">
+            <main className="w-full lg:h-[650px] h-[500px] relative sm:p-20 px-[5%] pt-[100px] mt-[45px] lg:pr-20 bg-mainSection bg-center bg-cover text-white">
                 <div className="flex flex-col">
                     <div className="lg:p-4 p-2 mt-5">
                         <h3 className="absolute z-10 lg:text-4xl sm:text-2xl text-xl py-4 px-2 ml-4 mt-5 border-y-4 border-r-4 border-green">
