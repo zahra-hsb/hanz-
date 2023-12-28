41ff777641a448ae0882432301eb7d433327f5cb
mohamad sd <mohamadsedighbakhshaish@gmail.com>
2023-12-27 21:41:00 -0800

    edit endSlider in home size

--- src/components/layout/Slider/index.jsx
+++ src/components/layout/Slider/index.jsx
@@ -6,18 +6,19 @@ const Slider = () => {
         <>
 
             <section className="flex flex-col w-full p-5 mb-36 mt-16">
-                <div className="flex items-center justify-between p-3 my-4">
-                    <div>
+                <div className="lg:w-[80%] lg:m-auto">
+                    <div className="flex items-center justify-between p-3 my-4">
+                        <div>
 
-                        <h3>پروژه های انجام شده</h3>
-                    </div>
-                    <div>
-                        <Button title={'درباره ما'} url={'/about'} border={'border-2 border-green'} color={'text-green text-sm'} />
+                            <h3>پروژه های انجام شده</h3>
+                        </div>
+                        <div>
+                            <Button title={'درباره ما'} url={'/about'} border={'border-2 border-green'} color={'text-green text-sm'} />
 
+                        </div>
                     </div>
-                </div>
-                <BigSlider slidesPerView={3} textSlider={true} textSlider1={'چرخه زندگی آلومینیوم'} textSlider2={'عایق صوتی: صدای ناخواسته داخل خانه را کاهش دهید'} textSlider3={'عایق صوتی: صدای ناخواسته داخل خانه را کاهش دهید'} textSlider4={'چرخه زندگی آلومینیوم'} sliderCount={''} />
-                {/* <div className="p-2">
+                    <BigSlider slidesPerView={3} textSlider={true} textSlider1={'چرخه زندگی آلومینیوم'} textSlider2={'عایق صوتی: صدای ناخواسته داخل خانه را کاهش دهید'} textSlider3={'عایق صوتی: صدای ناخواسته داخل خانه را کاهش دهید'} textSlider4={'چرخه زندگی آلومینیوم'} sliderCount={''} />
+                    {/* <div className="p-2">
                     <span>2</span>
                     <span>2</span>
                 </div>
@@ -35,6 +36,7 @@ const Slider = () => {
                         <div className="p-3">2</div>
                     </div>
                 </div> */}
+                </div>
             </section>
         </>
     )
