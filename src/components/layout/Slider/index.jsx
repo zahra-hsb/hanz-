e63d93d1035c36de923964a4263814e1b0fb89ae
mohamad sd <mohamadsedighbakhshaish@gmail.com>
2023-12-29 22:41:34 -0800

    optimize code in slider

--- src/components/layout/Slider/index.jsx
+++ src/components/layout/Slider/index.jsx
@@ -1,5 +1,8 @@
 import Button from "../Main/button"
 import BigSlider from "./BigSlider"
+import image1 from '../../../../public/image/image 6.svg'
+import image2 from '../../../../public/image/image 7.svg'
+import image3 from '../../../../public/image/image 8.svg'
 
 const Slider = () => {
     return (
@@ -17,25 +20,7 @@ const Slider = () => {
 
                         </div>
                     </div>
-                    <BigSlider slidesPerView={3} textSlider={true} textSlider1={'چرخه زندگی آلومینیوم'} textSlider2={'عایق صوتی: صدای ناخواسته داخل خانه را کاهش دهید'} textSlider3={'عایق صوتی: صدای ناخواسته داخل خانه را کاهش دهید'} textSlider4={'چرخه زندگی آلومینیوم'} sliderCount={''} />
-                    {/* <div className="p-2">
-                    <span>2</span>
-                    <span>2</span>
-                </div>
-                <div className="flex items-center justify-center">
-                    <div>
-                        <div>2</div>
-                        <div className="p-3">2</div>
-                    </div>
-                    <div>
-                        <div>2</div>
-                        <div className="p-3">2</div>
-                    </div>
-                    <div>
-                        <div>2</div>
-                        <div className="p-3">2</div>
-                    </div>
-                </div> */}
+                    <BigSlider slidesPerView={3} image1={image1} image2={image2} image3={image3} textSlider={true} textSlider1={'چرخه زندگی آلومینیوم'} textSlider2={'عایق صوتی: صدای ناخواسته داخل خانه را کاهش دهید'} textSlider3={'عایق صوتی: صدای ناخواسته داخل خانه را کاهش دهید'} textSlider4={'چرخه زندگی آلومینیوم'} sliderCount={''} />
                 </div>
             </section>
         </>
