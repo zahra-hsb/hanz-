3d9ee7fab6b1d36b79748d6de7577ba35088f2ed
mohamad sd <mohamadsedighbakhshaish@gmail.com>
2023-12-30 02:22:42 -0800

    add link to login in sidebar

--- src/components/layout/Sidebar/utils-sidebar/Login.jsx
+++ src/components/layout/Sidebar/utils-sidebar/Login.jsx
@@ -1,10 +1,14 @@
+import Link from "next/link";
+
 const Login = () => {
     return (
-        <div className="flex text-center bg-green h-[64px] text-lg text-white">
-            <div className="flex items-center justify-center basis-[100%]">
-                <p>ورود / ثبت نام</p>
+        <Link href='https://hanzalu.com/erp/'>
+            <div className="flex text-center bg-green h-[64px] text-lg text-white">
+                <div className="flex items-center justify-center basis-[100%]">
+                    <p>ورود به پنل فروش</p>
+                </div>
             </div>
-        </div>
+        </Link>
     )
 }
 
