84e1ac7779b3ea26b64aa293af15b2b7e48dec5f
mohamad sd <mohamadsedighbakhshaish@gmail.com>
2023-12-30 22:49:50 -0800

    edit hight for pages component

--- src/pages/contactUs/index.js
+++ src/pages/contactUs/index.js
@@ -1,6 +1,6 @@
 export default function ContactUs() {
   return (
-    <main className="flex justify-center items-center h-full border-2 mt-[50px] pb-[50px] h-[100vh]">
+    <main className="flex justify-center items-center border-2 mt-[50px] pb-[50px] h-[100vh]">
       <h2>Contact us</h2>
       {/* <div className={styles.a}></div>
       <div className={styles.b}></div> */}
--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,7 +1,7 @@
 import styles from "./products.module.css";
 export default function Products() {
   return (
-    <main className="flex justify-center items-center h-full border-2 mt-[50px] pb-[50px] h-[100vh]">
+    <main className="flex justify-center items-center border-2 mt-[50px] pb-[50px] h-[100vh]">
       <h2>Products</h2>
       {/* <div className={styles.a}></div>
       <div className={styles.b}></div> */}
