62d6bad5b8503bea5fb37f78a576515e90ba81a5
mohamad sd <mohamadsedighbakhshaish@gmail.com>
2023-12-30 20:54:34 -0800

    edit NextConfig

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   output: "export",
-};
-
-module.exports = nextConfig;
-
-module.exports = {
   images: {
     unoptimized: true,
   },
 };
+
+module.exports = nextConfig;
