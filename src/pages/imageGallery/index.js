48f22cae9f573191928376c009c9bbb95100270d
zahra <zahra20hb@gmail.com>
2024-01-03 12:46:24 +0330

    mobile responsive design created

--- src/pages/imageGallery/index.js
+++ src/pages/imageGallery/index.js
@@ -1,13 +1,24 @@
 import Main from "@/components/globalComponents/Main";
+import Image from "next/image"
+import image1 from "../../../public/image/Shayco-Ghorfe-WEBSITE.jpg"
 
 export default function ImageGallery() {
   return (
     <main className="flex flex-col">
-      <Main title={'گالری تصاویر'} bg={'bg-gallerySection'} height={'h-[390px]'} display={'hidden'}/>
-      <section>
-        <div>
-          <div>
-            {/* <Image src={} alt={'hanz'} /> */}
+      <Main title={'گالری تصاویر'} bg={'bg-gallerySection'} height={'h-[390px]'} display={'hidden'} />
+      <section className="p-10">
+        <div className="flex flex-wrap gap-16 p-5">
+          <div className="flex flex-col items-center justify-between gap-5">
+            <Image src={image1} alt={'hanz'} />
+            <h3>تصاویر نمایشگاه</h3>
+          </div>
+          <div className="flex flex-col items-center justify-between gap-5">
+            <Image src={image1} alt={'hanz'} />
+            <h3>تصاویر نمایشگاه</h3>
+          </div>
+          <div className="flex flex-col items-center justify-between gap-5">
+            <Image src={image1} alt={'hanz'} />
+            <h3>تصاویر نمایشگاه</h3>
           </div>
         </div>
       </section>
