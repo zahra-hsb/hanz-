04ccbf0f398ad99dde5d7f56d13ac691bdd2f64e
mohamad sd <mohamadsedighbakhshaish@gmail.com>
2024-01-03 00:38:01 -0800

    edit some filles

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,7 @@ import FaQ from "@/components/homeComponents/FaQ";
 import Main from "@/components/globalComponents/Main";
 import Slider from "@/components/homeComponents/Carousel";
 import Loading from "@/components/loading";
-// import Loading from "@/components/loading";
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 
 export default function Home() {
@@ -22,7 +21,6 @@ export default function Home() {
   return (
     <main>
       {/* {isLoading && <Loading />} */}
-      <Loading />
 
       <Main title={'سیستم های نوین درب و پنجره'} bg={'bg-mainSection'} text={'ما با هم ساختمان های پایدار با راه حل های آلومینیومی با کیفیت بالا ایجاد می کنیم.'} height={'lg:h-[650px] h-[500px] '} />
       <CategorySection />
