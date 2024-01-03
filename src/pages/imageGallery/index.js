d7e80b7cfdf05ce41c00dd6eb83c71c71597210c
zahra <zahra20hb@gmail.com>
2024-01-03 12:13:35 +0330

    elements added for image gallery

--- src/pages/imageGallery/index.js
+++ src/pages/imageGallery/index.js
@@ -4,6 +4,16 @@ export default function ImageGallery() {
   return (
     <main className="flex flex-col">
       <Main title={'گالری تصاویر'} bg={'bg-gallerySection'} height={'h-[390px]'} display={'hidden'}/>
+      <section>
+        <div>
+          <div>
+            {/* <Image src={} alt={'hanz'} /> */}
+          </div>
+        </div>
+      </section>
+      <section>
+
+      </section>
     </main>
   );
 }
