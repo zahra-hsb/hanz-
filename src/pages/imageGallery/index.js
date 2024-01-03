6f5b5f077eb36f16d04b5310e114e084259f9719
zahra <zahra20hb@gmail.com>
2024-01-03 16:55:12 +0330

    some problem fixed

--- src/pages/imageGallery/index.js
+++ src/pages/imageGallery/index.js
@@ -6,7 +6,7 @@ import image2 from "../../../public/image/Dominion-Tower-14.jpg"
 export default function ImageGallery() {
   return (
     <main className="flex flex-col">
-      <Main title={'گالری تصاویر'} bg={'bg-gallerySection'} height={'h-[390px]'} display={'hidden'} />
+      <Main title={'گالری تصاویر'} bg={'bg-aboutSection'} height={'h-[390px]'} display={'hidden'} />
       <section className="p-10">
         <div className="flex items-center justify-center flex-wrap gap-16 p-5">
           <div className="flex flex-col items-center justify-between gap-5 cursor-pointer overflow-hidden lg:w-[28%]">
@@ -15,18 +15,18 @@ export default function ImageGallery() {
             <h3 className="hover:text-green">تصاویر نمایشگاه</h3>
           </div>
           <div className="flex flex-col items-center justify-between gap-5 cursor-pointer overflow-hidden lg:w-[28%]">
-            <Image src={image2} alt={'hanz'} className="h-[178px] transition duration-300 ease-in-out overflow-hidden hover:scale-105" />
+            <Image src={image1} alt={'hanz'} className="transition duration-300 ease-in-out overflow-hidden hover:scale-105" />
             <h3 className="hover:text-green">تصاویر پروژه ها</h3>
           </div>
           <div className="flex flex-col items-center justify-between gap-5 cursor-pointer overflow-hidden lg:w-[28%]">
-            <Image src={image1} alt={'hanz'} className="h-[178px] transition duration-300 ease-in-out overflow-hidden hover:scale-105" />
+            <Image src={image1} alt={'hanz'} className="transition duration-300 ease-in-out overflow-hidden hover:scale-105" />
             <h3 className="hover:text-green">تصاویر نمایشگاه</h3>
           </div>
         </div>
       </section>
 
-      <section className="absolute w-full h-[500px] bg-[#eee] backdrop-blur-lg">
-      
+      <section className="w-full h-[500px] bg-[#eee] backdrop-blur-lg">
+
       </section>
 
     </main>
