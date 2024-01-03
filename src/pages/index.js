3351e2fe1710632abcd333bdb93173cdc9c36598
mohamad sd <mohamadsedighbakhshaish@gmail.com>
2024-01-02 23:48:05 -0800

    edit loading component

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Company from "@/components/homeComponents/Company";
 import FaQ from "@/components/homeComponents/FaQ";
 import Main from "@/components/globalComponents/Main";
 import Slider from "@/components/homeComponents/Carousel";
+import Loading from "@/components/loading";
 // import Loading from "@/components/loading";
 // import { useEffect, useState } from "react";
 
@@ -21,6 +22,7 @@ export default function Home() {
   return (
     <main>
       {/* {isLoading && <Loading />} */}
+      <Loading />
 
       <Main title={'سیستم های نوین درب و پنجره'} bg={'bg-mainSection'} text={'ما با هم ساختمان های پایدار با راه حل های آلومینیومی با کیفیت بالا ایجاد می کنیم.'} height={'lg:h-[650px] h-[500px] '} />
       <CategorySection />
