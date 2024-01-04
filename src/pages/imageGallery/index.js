e42602f4ecad9d55dbe444e89d4dbef22422ce3c
zahra <zahra20hb@gmail.com>
2024-01-04 09:35:04 +0330

    modal elements created

--- src/pages/imageGallery/index.js
+++ src/pages/imageGallery/index.js
@@ -10,22 +10,22 @@ export default function ImageGallery() {
       <section className="p-10">
         <div className="flex items-center justify-center flex-wrap gap-16 p-5">
           <div className="flex flex-col items-center justify-between gap-5 cursor-pointer overflow-hidden lg:w-[28%]">
-            <Image src={image1} alt={'hanz'} className="transition duration-300 ease-in-out overflow-hidden hover:scale-105" />
+            <Image src={image1} alt={'hanz'} className="transition duration-300 ease-in-out overflow-hidden hover:scale-105 hover:brightness-50" />
             {/* <div className="absolute top-0 left-0 w-full h-[180px] bg-black opacity-0 hover:opacity-60"></div> */}
             <h3 className="hover:text-green">تصاویر نمایشگاه</h3>
           </div>
           <div className="flex flex-col items-center justify-between gap-5 cursor-pointer overflow-hidden lg:w-[28%]">
-            <Image src={image1} alt={'hanz'} className="transition duration-300 ease-in-out overflow-hidden hover:scale-105" />
+            <Image src={image1} alt={'hanz'} className="transition duration-300 ease-in-out overflow-hidden hover:scale-105 hover:brightness-50" />
             <h3 className="hover:text-green">تصاویر پروژه ها</h3>
           </div>
           <div className="flex flex-col items-center justify-between gap-5 cursor-pointer overflow-hidden lg:w-[28%]">
-            <Image src={image1} alt={'hanz'} className="transition duration-300 ease-in-out overflow-hidden hover:scale-105" />
+            <Image src={image1} alt={'hanz'} className="transition duration-300 ease-in-out overflow-hidden hover:scale-105 hover:brightness-50" />
             <h3 className="hover:text-green">تصاویر نمایشگاه</h3>
           </div>
         </div>
       </section>
-
-      <section className="w-full h-[500px] bg-[#eee] backdrop-blur-lg">
+      <div className="fixed top-0 left-0 z-40 w-full h-full bg-black opacity-50"></div>
+      <section className="fixed top-[50%] left-[50%] ml-[-500px] mt-[-300px] z-50 w-[1000px] h-[600px] bg-gradient-to-r from-black to-[#252525]">
 
       </section>
 
