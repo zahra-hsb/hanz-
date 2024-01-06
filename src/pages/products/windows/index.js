bfb146d60991816fce7e585a8a081aa5af3c7657
mohamad sd <mohamadsedighbakhshaish@gmail.com>
2024-01-06 14:10:00 +0330

    created windows page in products

---
+++ src/pages/products/windows/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const windows = () => {
+    return (
+        <div className='flex justify-center items-center border-2 mt-[50px] pb-[50px] h-[100vh]'>
+            windows
+        </div>
+    );
+}
+
+export default windows;
