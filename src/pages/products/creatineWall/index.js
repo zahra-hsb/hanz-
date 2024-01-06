0cedc0d86a1c1b5d417c10b3a155ec1e50c094c2
mohamad sd <mohamadsedighbakhshaish@gmail.com>
2024-01-06 14:49:46 +0330

    delete door and windows and creatineWall pages in products

--- src/pages/products/creatineWall/index.js
+++
@@ -1,9 +0,0 @@
-const CreatineWall = () => {
-    return (
-        <div className='flex justify-center items-center border-2 mt-[50px] pb-[50px] h-[100vh]'>
-            creatineWall
-        </div>
-    );
-}
-
-export default CreatineWall;
--- src/pages/products/door/index.js
+++
@@ -1,9 +0,0 @@
-const Door = () => {
-    return (
-        <div className='flex justify-center items-center border-2 mt-[50px] pb-[50px] h-[100vh]'>
-            Door
-        </div>
-    );
-}
-
-export default Door;
