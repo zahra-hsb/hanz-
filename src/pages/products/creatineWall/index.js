f48dc7b68744ba62afb246098103cc0698fabc70
mohamad sd <mohamadsedighbakhshaish@gmail.com>
2024-01-06 14:17:08 +0330

    created door page in products

---
+++ src/pages/products/creatineWall/index.js
@@ -0,0 +1,9 @@
+const CreatineWall = () => {
+    return (
+        <div className='flex justify-center items-center border-2 mt-[50px] pb-[50px] h-[100vh]'>
+            creatineWall
+        </div>
+    );
+}
+
+export default CreatineWall;
