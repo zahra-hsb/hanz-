3a1de8979bc17d796880db690cc0fbc25f68d3ae
mohamad sd <mohamadsedighbakhshaish@gmail.com>
2024-01-06 14:14:21 +0330

    created door page in products

---
+++ src/pages/products/door/index.js
@@ -0,0 +1,9 @@
+const Door = () => {
+    return (
+        <div className='flex justify-center items-center border-2 mt-[50px] pb-[50px] h-[100vh]'>
+            Door
+        </div>
+    );
+}
+
+export default Door;
