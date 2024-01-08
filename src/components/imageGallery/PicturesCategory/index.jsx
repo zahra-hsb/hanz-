21c506bb66866e53c2aa9f1a5376dbe2338733b1
zahra <zahra20hb@gmail.com>
2024-01-08 09:53:36 +0330

    space for show more button created

--- src/components/imageGallery/PicturesCategory/index.jsx
+++ src/components/imageGallery/PicturesCategory/index.jsx
@@ -20,17 +20,17 @@ const PicturesGallery = () => {
                 <div className="flex items-center justify-center flex-wrap gap-16 p-5">
                     <div className="group flex flex-col items-center justify-between gap-5 relative cursor-pointer overflow-hidden lg:w-[28%]">
                         <Image src={image1} alt={'hanz'} className="transition duration-300 ease-in-out overflow-hidden hover:scale-105 hover:brightness-50" />
-                        <Button title={'نمایش بیشتر'} url={''} border={'opacity-0 transition-all duration-300 ease-in-out group-hover:opacity-100 border-2 border-white absolute mt-[80px]'} color={'text-white'} hover={'hover:text-black hover:bg-white'} onClick={showModal} />
+                        <Button title={'نمایش بیشتر'} url={''} border={'opacity-0 transition-all duration-300 ease-in-out group-hover:opacity-100 border-2 border-white absolute sm:mt-[30%] mt-[28%]'} color={'text-white'} hover={'hover:text-black hover:bg-white'} onClick={showModal} />
                         <h3 className="hover:text-green">تصاویر نمایشگاه</h3>
                     </div>
                     <div className="group flex flex-col items-center justify-between gap-5 relative cursor-pointer overflow-hidden lg:w-[28%]">
                         <Image src={image1} alt={'hanz'} className="transition duration-300 ease-in-out overflow-hidden hover:scale-105 hover:brightness-50" />
-                        <Button title={'نمایش بیشتر'} url={''} border={'opacity-0 transition-all duration-300 ease-in-out group-hover:opacity-100 border-2 border-white absolute mt-[80px]'} color={'text-white'} hover={'hover:text-black hover:bg-white'} onClick={showModal} />
+                        <Button title={'نمایش بیشتر'} url={''} border={'opacity-0 transition-all duration-300 ease-in-out group-hover:opacity-100 border-2 border-white absolute sm:mt-[30%] mt-[28%]'} color={'text-white'} hover={'hover:text-black hover:bg-white'} onClick={showModal} />
                         <h3 className="hover:text-green">تصاویر پروژه ها</h3>
                     </div>
                     <div className="group flex flex-col items-center justify-between gap-5 relative cursor-pointer overflow-hidden lg:w-[28%]">
                         <Image src={image1} alt={'hanz'} className="transition duration-300 ease-in-out overflow-hidden hover:scale-105 hover:brightness-50" />
-                        <Button title={'نمایش بیشتر'} url={''} border={'opacity-0 transition-all duration-300 ease-in-out group-hover:opacity-100 border-2 border-white absolute mt-[80px]'} color={'text-white'} hover={'hover:text-black hover:bg-white'} onClick={showModal} />
+                        <Button title={'نمایش بیشتر'} url={''} border={'opacity-0 transition-all duration-300 ease-in-out group-hover:opacity-100 border-2 border-white absolute sm:mt-[30%] mt-[28%]'} color={'text-white'} hover={'hover:text-black hover:bg-white'} onClick={showModal} />
                         <h3 className="hover:text-green">تصاویر نمایشگاه</h3>
                     </div>
                 </div>
