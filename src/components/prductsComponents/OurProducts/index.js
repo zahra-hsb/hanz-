5c8d515834ff274fbe9a68e505928e396f15c944
mohamad sd <mohamadsedighbakhshaish@gmail.com>
2024-01-13 13:35:50 +0330

    add Profile component

--- src/components/prductsComponents/OurProducts/index.js
+++ src/components/prductsComponents/OurProducts/index.js
@@ -15,6 +15,7 @@ import curtainWall3 from '../../../../public/image/curtainWall3.jpg'
 import Button from "@/components/globalComponents/Main/button";
 import MainSection from "../MainSection";
 import Main from "@/components/globalComponents/Main";
+import Profile from "../profile";
 
 
 export default function OurProducts() {
@@ -37,21 +38,7 @@ export default function OurProducts() {
           </div>
         </div>
 
-        <div id="profile"></div>
-        <div className="w-full">
-          <div className="mt-[90px] mb-10 flex justify-between items-center text-xl text-gray-600 w-[90%] sm:w-[100%] m-auto">
-            <div>
-              <h2>پروفیل</h2>
-            </div>
-
-            <div>
-              <Button title={'بیشتر'} url={'/products'} border={'border-2 border-green'} hover={'hover:bg-green hover:text-white'} />
-            </div>
-          </div>
-          <div id="home-carousel" className="w-full">
-            <Slider url={'/products'} slidesPerView={3} image1={profile1} image2={profile3} image3={profile2} image4={profile3} textSlider={true} textSlider1={'System 140-SLIM'} textSlider2={'System SL-95T'} textSlider3={'System 95T-MONO'} textSlider4={'System SL-95T'} sliderCount={''} />
-          </div>
-        </div>
+        <Profile />
 
         <div id="profile-properties"></div>
         <div className="w-full">
