cbfc884f4464beab39efcabea04d672a630ecc86
zahra <zahra20hb@gmail.com>
2024-01-14 16:22:11 +0330

    swiper slider created

--- src/components/imageGallery/GalleryModal/index.jsx
+++ src/components/imageGallery/GalleryModal/index.jsx
@@ -7,17 +7,18 @@ import arrowRight from "../../../../public/icons/arrow-1.svg"
 import arrowLeft from "../../../../public/icons/arrow.svg"
 import Slider from "@/components/globalComponents/Slider"
 
-import {Pagination, Navigation} from 'swiper/modules'
-import {Swiper, SwiperSlide} from 'swiper/react'
+import { Pagination, Navigation } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/react'
 
 import 'swiper/css';
 import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 const GalleryModal = ({ closeModal }) => {
     return (
         <>
             <div className="transition duration-100 fixed top-0 left-0 z-40 w-full h-full bg-black cursor-pointer opacity-90" onClick={closeModal}></div>
-            <section className="transition duration-100 fixed top-[50%] left-[50%] p-16 ml-[-500px] mt-[-300px] z-50 w-[1000px] h-[600px] bg-gradient-to-tr from-[#010101] to-[#252525]">
+            <section className="transition duration-100 fixed top-[50%] left-[50%] p-16 ml-[-40vw] mt-[-45vh] z-50 w-[80vw] h-[90vh] bg-gradient-to-tr from-[#010101] to-[#252525]">
                 <div className="flex items-center justify-center">
                     <div>
                         <Image src={mainPic} alt="" width={400} className="inner drop-shadow-sm" />
@@ -40,6 +41,29 @@ const GalleryModal = ({ closeModal }) => {
                 </div> */}
 
                 {/* <Slider url={'#'} slidesPerView={3} image1={`${primePic1}`} image2={`${primePic2}`} image3={`${primePic1}`} image4={`${primePic2}`} /> */}
+                <div className="p-20">
+                    <Swiper
+                        modules={[Navigation, Pagination]}
+                        spaceBetween={10}
+                        slidesPerView={3}
+                        navigation
+                        pagination
+                        className="mySwiper text-white"
+                    >
+                        <SwiperSlide>
+                            <Image src={primePic1} alt="" width={400} className="inner drop-shadow-sm" />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <Image src={mainPic} alt="" width={400} className="inner drop-shadow-sm" />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <Image src={primePic2} alt="" width={400} className="inner drop-shadow-sm" />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <Image src={primePic1} alt="" width={400} className="inner drop-shadow-sm" />
+                        </SwiperSlide>
+                    </Swiper>
+                </div>
 
             </section>
         </>
