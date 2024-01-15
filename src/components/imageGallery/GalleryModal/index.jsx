80b06c86ed9500814208110f814f3f95b40b5433
zahra <zahra20hb@gmail.com>
2024-01-15 10:17:27 +0330

    loop for images added and some code cleared

--- src/components/imageGallery/GalleryModal/index.jsx
+++ src/components/imageGallery/GalleryModal/index.jsx
@@ -3,9 +3,7 @@ import Image from "next/image"
 import mainPic from "../../../../public/image/Modiriyat-WEBSITE.jpg"
 import primePic1 from "../../../../public/image/Sales-Manager-WEBSITE.jpg"
 import primePic2 from "../../../../public/image/sales-manager-WEBSITE-1.jpg"
-import arrowRight from "../../../../public/icons/arrow-1.svg"
-import arrowLeft from "../../../../public/icons/arrow.svg"
-import Slider from "@/components/globalComponents/Slider"
+
 
 
 import { Pagination, Navigation } from 'swiper/modules'
@@ -35,23 +33,8 @@ const GalleryModal = ({ closeModal, opacity, wraperOpacity }) => {
                         
                     </div>
                 </div>
-                {/* <div className="flex items-center justify-between py-16">
-                    <Image src={arrowRight} alt="arrow-right" className="cursor-pointer hover:invert" />
-                    <div className="lg:w-full flex items-center justify-between px-16">
-                        <div className="">
-                            <Image src={primePic1} alt="" width={160} className="transition duration-300 ease-in-out hover:scale-105 hover:brightness-50 cursor-pointer" />
-                        </div>
-                        <div className="w-full">
-                            <Image src={mainPic} alt="" width={200} className="transition duration-300 ease-in-out hover:scale-105 hover:brightness-50 cursor-pointer" />
-                        </div>
-                        <div>
-                            <Image src={primePic2} alt="" width={160} className="transition duration-300 ease-in-out hover:scale-105 hover:brightness-50 cursor-pointer" />
-                        </div>
-                        <Image src={arrowLeft} alt="arrow-right" className="cursor-pointer hover:invert" />
-                    </div>
-                </div> */}
 
-                {/* <Slider url={'#'} slidesPerView={3} image1={`${primePic1}`} image2={`${primePic2}`} image3={`${primePic1}`} image4={`${primePic2}`} /> */}
+
                 <div className="p-20">
                     <Swiper
                         modules={[Navigation, Pagination]}
--- src/components/imageGallery/PicturesCategory/index.jsx
+++ src/components/imageGallery/PicturesCategory/index.jsx
@@ -5,7 +5,13 @@ import image2 from "../../../../public/image/Dominion-Tower-14.jpg"
 import Button from "@/components/globalComponents/Main/button"
 import { useState } from "react"
 
+
 const PicturesGallery = () => {
+    const galleryArray = [
+        {src: image1, category: 'تصاویر نمایشگاه'},
+        {src: image2, category: 'تصاویر نمایشگاه'},
+    ]
+    console.log(galleryArray[1].src)
     const [isShow, setShow] = useState(false)
     console.log('show: ', isShow)
     function showModal() {
@@ -14,39 +20,26 @@ const PicturesGallery = () => {
     function closeModal() {
         setShow(false)
     }
+
     return (
         <>
 
             <section className="p-10">
                 <div className="flex items-center justify-center flex-wrap sm:flex-nowrap gap-16 p-5">
-                    <div className="group flex flex-col items-center justify-between gap-5 relative cursor-pointer overflow-hidden lg:w-[28%]" onClick={() => showModal()}>
-                        <div className="w-full lg:h-[160px] 2xl:h-[250px] overflow-hidden">
-                            <Image src={image1} alt={'hanz'} className="transition w-full duration-300 ease-in-out overflow-hidden hover:scale-105 hover:brightness-50" />
-                        </div>
-                        <button className='z-50 text-green text-sm lg:text-md px-4 py-1 rounded-3xl transition-colors opacity-0 duration-300 ease-in-out group-hover:opacity-100 border-2 border-white absolute sm:my-[30%] my-auto mx-auto text-white hover:text-black hover:bg-white' onClick={() => showModal()} >نمایش بیشتر  ←</button>
-                        <h3 className="hover:text-green">تصاویر نمایشگاه</h3>
-                    </div>
-                    <div className="group flex flex-col items-center justify-between gap-5 relative cursor-pointer overflow-hidden lg:w-[28%]" onClick={() => showModal()}>
-                        <div className="w-full lg:h-[160px] 2xl:h-[250px] overflow-hidden">
-                            <Image src={image1} alt={'hanz'} className="transition w-full duration-300 ease-in-out overflow-hidden hover:scale-105 hover:brightness-50" />
-
-                        </div>
-                        <button className='z-50 text-green text-sm lg:text-md px-4 py-1 rounded-3xl transition-colors opacity-0 duration-300 ease-in-out group-hover:opacity-100 border-2 border-white absolute sm:my-[30%] mt-[28%]  my-auto mx-auto text-white hover:text-black hover:bg-white' onClick={() => showModal()} >نمایش بیشتر  ←</button>
-
-                        <h3 className="hover:text-green">تصاویر پروژه ها</h3>
-                    </div>
-                    <div className="group flex flex-col items-center justify-between gap-5 relative cursor-pointer overflow-hidden lg:w-[28%]" onClick={() => showModal()}>
-                        <div className="w-full lg:h-[160px] 2xl:h-[250px] overflow-hidden ">
-                            <Image src={image1} alt={'hanz'} className="transition w-full duration-300 ease-in-out overflow-hidden hover:scale-105 hover:brightness-50" />
-
-                        </div>
-                        <button className='z-50 text-green text-sm lg:text-md px-4 py-1 rounded-3xl transition-colors opacity-0 duration-300 ease-in-out group-hover:opacity-100 border-2 border-white absolute sm:my-[30%] mt-[28%] text-white hover:text-black hover:bg-white' onClick={() => showModal()} >نمایش بیشتر  ←</button>
-
-                        <h3 className="hover:text-green">تصاویر نمایشگاه</h3>
-                    </div>
+                    {galleryArray.map((img, index) => (
+                        <>
+                            <div className="group flex flex-col items-center justify-between gap-5 relative cursor-pointer overflow-hidden lg:w-[28%]" onClick={() => showModal()}>
+                                <div className="w-full lg:h-[160px] 2xl:h-[250px] overflow-hidden">
+                                    <Image src={img.src} key={index} alt={'hanz'} className="transition w-full duration-300 ease-in-out overflow-hidden hover:scale-105 hover:brightness-50" />
+                                </div>
+                                <button className='z-50 text-green text-sm lg:text-md px-4 py-1 rounded-3xl transition-colors opacity-0 duration-300 ease-in-out group-hover:opacity-100 border-2 border-white absolute sm:my-[30%] my-auto mx-auto text-white hover:text-black hover:bg-white' onClick={() => showModal()} >نمایش بیشتر  ←</button>
+                                <h3 className="hover:text-green">تصاویر نمایشگاه</h3>
+                            </div>
+                        </>
+                    ))}
                 </div>
             </section>
-            {isShow ? <GalleryModal closeModal={closeModal} opacity={'opacity-100 z-50'} wraperOpacity={'opacity-90 z-40'} /> :  <GalleryModal closeModal={closeModal} opacity={'opacity-0 z-[-10]'} wraperOpacity={'opacity-0 z-[-10]'} />}
+            {isShow ? <GalleryModal closeModal={closeModal} opacity={'opacity-100 z-50'} wraperOpacity={'opacity-90 z-40'} /> : <GalleryModal closeModal={closeModal} opacity={'opacity-0 z-[-10]'} wraperOpacity={'opacity-0 z-[-10]'} />}
         </>
     )
 }
