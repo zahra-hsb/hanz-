5dc9887b627e641de6430ce1bc3a4a8695d25ace
mohamad sd <mohamadsedighbakhshaish@gmail.com>
2024-01-17 09:13:36 +0330

    edit menu

--- src/components/layout/Sidebar/utils-sidebar/Menu.jsx
+++ src/components/layout/Sidebar/utils-sidebar/Menu.jsx
@@ -1,8 +1,8 @@
 import Link from "next/link"
 import { useState } from "react";
 
-const Menu = ({ selectedTab, handleTabClick, setSidebarShow }) => {
-    // const [hiddenChildMenu, setHiddenChildMenu] = useState(true);
+const Menu = ({ selectedTab, handleTabClick, setSidebarShow, toggleOverflow }) => {
+    const [showChildMenu, setShowChildMenu] = useState(false);
 
     const menuItem = [
         { tab: 'home', label: 'خانه' },
@@ -19,50 +19,66 @@ const Menu = ({ selectedTab, handleTabClick, setSidebarShow }) => {
                     key={item.tab}
                     onClick={() => setSidebarShow(false)}
                 >
-                    <Link
-                        href={item.tab === 'home' ? '/' : item.tab}
-                        className={`${selectedTab === item.tab ? 'text-green' : 'text-gray-500'} flex items-center p-2 rounded-lg hover:bg-gray-100 group hover:text-green`}
-                        onClick={() => {
-                            handleTabClick(item.tab)
-                            toggleOverflow()
-                        }}
-                        // onMouseOver={() => item.tab === 'products' ? setHiddenChildMenu(false) : ''}
-                        // onMouseLeave={() => item.tab === 'products' ? setHiddenChildMenu(true) : ''}
-                    >
-                        <span className="nav-link ms-3 pl-2">{item.label}</span>
-                        {/* {item.tab === 'products' && (
-                            <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
-                            </svg>
-                        )} */}
-                    </Link>
-                    {/* {item.tab === 'products' && (
+                    <div className="flex items-center justify-between">
+                        <Link
+                            href={item.tab === 'home' ? '/' : item.tab}
+                            className={`${selectedTab === item.tab ? 'text-green' : 'text-gray-500'} flex items-center w-full p-2 rounded-lg hover:bg-gray-100 group hover:text-green`}
+                            onClick={() => {
+                                handleTabClick(item.tab)
+                                // toggleOverflow()
+                            }}
+                        >
+                            <span className="nav-link ms-3">{item.label}</span>
+                        </Link>
+                        {item.tab === 'products' && (
+                            <ArowDown setShowChildMenu={setShowChildMenu} />
+                        )}
+                    </div>
+                    {item.tab === 'products' && (
                         <ul
                             id="dropdown-example"
-                            className={`${hiddenChildMenu ? 'hidden' : ''} py-2 space-y-2`}
-                            onMouseOver={() => setHiddenChildMenu(false)}
-                            onMouseLeave={() => setHiddenChildMenu(true)}
+                            className={`${!showChildMenu ? 'hidden' : ''} py-2 space-y-2`}
                         >
                             <li>
-                                <Link href="#" className={`text-gray-500 flex items-center p-2 pr-8 rounded-lg hover:bg-gray-100 group hover:text-green`}>همه محصولات</Link>
-                                <Link
-                                    href="#"
-                                    className={`text-gray-500 flex items-center p-2 pr-8 rounded-lg hover:bg-gray-100 group hover:text-green`}
-                                    onClick={() => handleTabClick(item.tab)}
-                                >پنجره</Link>
+                                <Link href="#" className={`text-gray-500 flex items-center p-2 pr-8 rounded-lg hover:bg-gray-100 group hover:text-green`}
+                                // onClick={() => handleTabClick(item.tab)}
+                                >ساختمانی</Link>
                             </li>
                             <li>
-                                <Link href="#" className={`text-gray-500 flex items-center p-2 pr-8 rounded-lg hover:bg-gray-100 group hover:text-green`}>درب</Link>
+                                <Link href="#" className={`text-gray-500 flex items-center p-2 pr-8 rounded-lg hover:bg-gray-100 group hover:text-green`}>فنی</Link>
                             </li>
                             <li>
-                                <Link href="#" className={`text-gray-500 flex items-center p-2 pr-8 rounded-lg hover:bg-gray-100 group hover:text-green`}>کرتین وال</Link>
+                                <Link href="#" className={`text-gray-500 flex items-center p-2 pr-8 rounded-lg hover:bg-gray-100 group hover:text-green`}>خدمات</Link>
+                            </li>
+                            <li>
+                                <Link href="#" className={`text-gray-500 flex items-center p-2 pr-8 rounded-lg hover:bg-gray-100 group hover:text-green`}>اکسسوری</Link>
                             </li>
                         </ul>
-                    )} */}
+                    )}
                 </li>
             ))}
         </ul>
     )
 }
 
+
 export default Menu;
+
+export const ArowDown = ({ setShowChildMenu }) => {
+    return (
+        <div
+            className="px-2 py-3 rounded-lg hover:bg-gray-100 group hover:text-green cursor-pointer"
+            onClick={() => setShowChildMenu(s => !s)}
+        >
+            <svg
+                className="w-3 h-3 text-gray-500"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 10 6"
+            >
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
+            </svg>
+        </div>
+    );
+}
