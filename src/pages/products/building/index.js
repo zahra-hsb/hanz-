a070e11fab7fc169e7f8d278f823910bbadc4a05
mohamad sd <mohamadsedighbakhshaish@gmail.com>
2024-01-17 12:35:44 +0330

    edit building page

--- src/pages/products/building/index.js
+++ src/pages/products/building/index.js
@@ -6,7 +6,7 @@ import Profile from "@/components/prductsComponents/Profile";
 import ProfileProperties from "@/components/prductsComponents/ProfileProperties";
 import WhyHanz from "@/components/prductsComponents/WhyHanz";
 
-export default function Products() {
+export default function building() {
   return (
     <main className="flex flex-col justify-center items-center pb-[50px]">
       <Main title={'ساختمانی'} bg={'bg-buildingSection'} height={'h-[390px]'} display={'hidden'} />
