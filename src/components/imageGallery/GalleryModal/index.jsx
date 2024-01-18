436da471056c76e8ac0150f5a8c4f6ab1b175228
zahra <zahra20hb@gmail.com>
2024-01-18 11:37:06 +0330

    slider paddings changed in mobile and tablet designs

--- src/components/imageGallery/GalleryModal/index.jsx
+++ src/components/imageGallery/GalleryModal/index.jsx
@@ -28,7 +28,7 @@ const GalleryModal = ({ mainPic, closeModal, opacity, wraperOpacity, galleryProp
             <span className="w-[15px] fixed top-0 right-0 z-50 p-5 sm:p-10 lg:p-10 text-4xl text-white cursor-pointer hover:text-red" onClick={closeModal}>
                 <span className="rotate-45 flex hover:text-red">+</span>
             </span>
-            <section className={`${opacity} rounded-md overflow-hidden transition duration-500 ease-in-out fixed sm:top-[50%] top-[10vh] sm:left-[50%] lg:p-16 sm:ml-[-40vw] sm:mt-[-45vh] w-[90%] sm:w-[80vw] sm:h-[90vh] bg-white`}>
+            <section className={`${opacity} rounded-md overflow-hidden transition duration-500 ease-in-out fixed sm:top-[50%] top-[10vh] sm:left-[50%] lg:p-10 sm:ml-[-40vw] sm:mt-[-45vh] w-[90%] sm:w-[80vw] sm:h-[90vh] bg-white`}>
                 <div>
                     <div className="flex items-center justify-center">
                         {picture && <Image src={picture} alt="" width={400} className="w-full lg:w-[45%] left-0 inner drop-shadow-lg cursor-zoom-in sm:h-[60vh] lg:h-full lg:rounded" />}
@@ -37,7 +37,7 @@ const GalleryModal = ({ mainPic, closeModal, opacity, wraperOpacity, galleryProp
                 </div>
 
 
-                <div className="py-8 sm:py-5 lg:p-20">
+                <div className="py-8 sm:py-5 lg:p-10">
                     <Swiper
                         modules={[Navigation, Pagination]}
                         spaceBetween={20}
