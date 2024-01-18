9800570c7041651f12a4cdafc866dfc0a3b3f021
mohamad sd <mohamadsedighbakhshaish@gmail.com>
2024-01-18 11:58:49 +0330

    edit header component

--- src/components/layout/Header/index.jsx
+++ src/components/layout/Header/index.jsx
@@ -58,7 +58,7 @@ const Header = ({ setSidebarShow, router }) => {
     return (
         <>
             <header className={`
-            ${router.pathname === '/products' || router.pathname === '/products/building' || router.pathname === '/products/industrial' || '/products/services' || '/products/accessory' ? 'h-[80px] sm:h-[50px]' : ''}
+            ${router.pathname === '/products' || router.pathname === '/products/building' || router.pathname === '/products/industrial' || router.pathname === '/products/services' || router.pathname === '/products/accessory' ? 'h-[80px] sm:h-[50px]' : 'h-[50px]'}
             h-[50px] fixed bg-white z-30 flex justify-start items-center w-full border-b`}>
                 <div className="flex flex-col sm:flex-row">
                     <div className="flex justify-start items-center">
