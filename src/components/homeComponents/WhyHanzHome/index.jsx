68c58da0866e979a1a2894887afa2f70825b91aa
mohamad sd <mohamadsedighbakhshaish@gmail.com>
2024-01-21 09:59:02 +0330

    edit home page

---
+++ src/components/homeComponents/WhyHanzHome/index.jsx
@@ -0,0 +1,12 @@
+
+const WhyHanzHome = () => {
+    return (
+        <section className="flex flex-col pt-5 w-full my-20">
+            <div className="flex w-full lg:w-[90%] lg:m-auto flex-col lg:flex-row items-center justify-between">
+                aaaaaaaaaaaaaa
+            </div>
+        </section>
+    );
+}
+
+export default WhyHanzHome; 
