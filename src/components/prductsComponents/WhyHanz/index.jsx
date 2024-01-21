55f358933cb92a4cc7afacc7502e8c8065f19dba
mohamad sd <mohamadsedighbakhshaish@gmail.com>
2024-01-21 14:20:15 +0330

    edit WhyHanz component

--- src/components/prductsComponents/WhyHanz/index.jsx
+++ src/components/prductsComponents/WhyHanz/index.jsx
@@ -5,19 +5,31 @@ import hanzProfile3 from '../../../../public/image/hanz-profile3.webp'
 
 
 export default function WhyHanz() {
+  const slides = [
+    { image: hanzProfile1, text: 'کنترل دقیق فرآیند' },
+    { image: hanzProfile3, text: 'ظرفیت تولید' },
+    { image: hanzProfile2, text: 'تجهیزات پیشرفته' },
+    { image: hanzProfile3, text: 'قاب منظره آلومینیومی' }
+  ]
+
   return (
     <>
-        <div className="w-full">
-          <div className="mt-[90px] mb-10 flex justify-between items-center text-xl text-gray-600 w-[90%] sm:w-[100%] m-auto">
-            <div>
-              <h2>چرا هانز</h2>
-            </div>
+      <div className="w-full">
+        <div className="mt-[90px] mb-10 flex justify-between items-center text-xl text-gray-600 w-[90%] sm:w-[100%] m-auto">
+          <div>
+            <h2>چرا هانز</h2>
+          </div>
 
-          </div>
-          <div id="home-carousel" className="w-full">
-            <Slider url={'/products'} slidesPerView={3} image1={hanzProfile1} image2={hanzProfile3} image3={hanzProfile2} image4={hanzProfile3} textSlider={true} textSlider1={'کنترل دقیق فرآیند'} textSlider2={'ظرفیت تولید'} textSlider3={'تجهیزات پیشرفته'} textSlider4={'قاب منظره آلومینیومی'} sliderCount={''} />
-          </div>
         </div>
+        <div id="home-carousel" className="w-full">
+          <Slider
+            url='/products'
+            slidesPerView={3}
+            slides={slides}
+            textSlider={true}
+          />
+        </div>
+      </div>
     </>
   );
 }
