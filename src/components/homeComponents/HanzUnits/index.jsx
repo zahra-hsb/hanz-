702f76cf4b8896af92a41f4b8c97647aaa030eb5
zahra <zahra20hb@gmail.com>
2024-01-21 10:44:24 +0330

    slides added to swiper slider

--- src/components/homeComponents/HanzUnits/index.jsx
+++ src/components/homeComponents/HanzUnits/index.jsx
@@ -1,24 +1,46 @@
 import Slider from "@/components/globalComponents/Slider"
+import Image from "next/image";
+import { Swiper, SwiperSlide } from 'swiper/react';
 
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
 
+// import required modules
+import { Pagination } from 'swiper/modules';
 
 const HanzUnits = () => {
     const sliderItems = [
-        {image: '', title: 'واحد طراحی سیستم (SYSTEM DESIGN LINE)'},
-        {image: '', title: 'واحد اکستروژن (EXTRUSION LINE)'},
-        {image: '', title: 'واحد رنگ دکورال (WOODEN POWDER COATING LINE)'},
-        {image: '', title: 'واحد رنگ پودری الکترواستاتیک (POWDER COATING LINE)'},
-        {image: '', title: 'واحد ترمال بریک (TERMAL BREAKING LINE)'},
-        {image: '', title: 'واحد مونتاژ (ASSEMBLING LINE)'},
-        {image: '', title: 'واحد رنگ آنادایز (ANODIZING LINE)'},
+        { image: '', text: 'واحد طراحی سیستم (SYSTEM DESIGN LINE)' },
+        { image: '', text: 'واحد اکستروژن (EXTRUSION LINE)' },
+        { image: '', text: 'واحد رنگ دکورال (WOODEN POWDER COATING LINE)' },
+        { image: '', text: 'واحد رنگ پودری الکترواستاتیک (POWDER COATING LINE)' },
+        { image: '', text: 'واحد ترمال بریک (TERMAL BREAKING LINE)' },
+        { image: '', text: 'واحد مونتاژ (ASSEMBLING LINE)' },
+        { image: '', text: 'واحد رنگ آنادایز (ANODIZING LINE)' },
     ]
     return (
         <>
             <section>
-                <Slider 
-                    url={'#'}
-                    slidesPerView={4}
-                />
+                <Swiper
+                    slidesPerView={3}
+                    spaceBetween={30}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[Pagination]}
+                    className="mySwiper"
+                >
+                    {sliderItems.map((item, index) => (
+                        <>
+                            <SwiperSlide>
+                                <Image src={item.image} alt="" key={index}/>
+                                <p>{item.text}</p>
+                            </SwiperSlide>
+                        </>
+                    ))}
+
+                </Swiper>
             </section>
         </>
     )
