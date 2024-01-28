25b7ce9edeab57cb2b8121a1ee78d561c8fde580
zahra <zahra20hb@gmail.com>
2024-01-28 09:14:09 +0330

    change component name and create accessories array

--- src/components/prductsComponents/MainSection/index.jsx
+++ src/components/prductsComponents/MainSection/index.jsx
@@ -1,5 +1,5 @@
 import GallerySlider from "@/components/imageGallery/GallerySlider";
-import Structural from "../Structural";
+import ProductCategory from "../ProductCategory";
 import hanzProfile1 from '../../../../public/image/hanz-profile1.webp'
 import hanzProfile2 from '../../../../public/image/hanz-profile2.webp'
 import hanzProfile3 from '../../../../public/image/hanz-profile3.webp'
@@ -25,6 +25,11 @@ export default function MainSection() {
     { image: hanzProfile2, text: 'واحد مونتاژ' },
     { image: hanzProfile2, text: 'واحد رنگ آنادیز' },
   ]
+  const Accessories = [
+    { image: hanzProfile1, text: 'پروفیل' },
+    { image: hanzProfile3, text: 'متعلقات' },
+    { image: hanzProfile2, text: 'کراتین وال' },
+  ]
   return (
     <>
 
@@ -43,14 +48,15 @@ export default function MainSection() {
         </div>
       </div>
 
-      <Structural items={structuralArray} subCategory={'ساختمانی'} 
+      <ProductCategory items={structuralArray} subCategory={'ساختمانی'} 
       text={'در شرکت ما تجهیزات ساختمانی باکیفیت توسط نیروی های ما ساخته می شود.'}/>
-      <Structural items={industrialArray} subCategory={'صنعتی'} 
+      <ProductCategory items={industrialArray} subCategory={'صنعتی'} 
         text={''}
       />
-      <Structural items={servicesArray}
+      <ProductCategory items={servicesArray}
       subCategory={'خدمات'}
       text={''} />
+      
     </>
   );
 }
