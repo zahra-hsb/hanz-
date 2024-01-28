ddf32a98e9c9ebd7b6a893aa2243a751ca83a0d0
mohamad sd <mohamadsedighbakhshaish@gmail.com>
2024-01-28 16:50:36 +0330

    edit Builing page

--- src/pages/products/building/index.js
+++ src/pages/products/building/index.js
@@ -104,11 +104,11 @@ export default function Builing() {
 
         <div className="flex justify-center mt-10">
           <nav aria-label="Page navigation example">
-            <ul class="flex items-center -space-x-px h-8 text-sm">
+            <ul className="flex items-center -space-x-px h-8 text-sm">
               <li>
-                <p onClick={() => currentPage > 1 && setCurrentPage(currentPage - 1)} class="cursor-pointer flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700">
-                  <span class="sr-only">Previous</span>
-                  <svg class="w-2.5 h-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <p onClick={() => currentPage > 1 && setCurrentPage(currentPage - 1)} className="cursor-pointer flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700">
+                  <span className="sr-only">Previous</span>
+                  <svg className="w-2.5 h-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
                   </svg>
                 </p>
@@ -119,9 +119,9 @@ export default function Builing() {
                 </li>
               ))}
               <li>
-                <p onClick={() => currentPage < totalPages && setCurrentPage(currentPage + 1)} class="cursor-pointer flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700">
-                  <span class="sr-only">Next</span>
-                  <svg class="w-2.5 h-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <p onClick={() => currentPage < totalPages && setCurrentPage(currentPage + 1)} className="cursor-pointer flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700">
+                  <span className="sr-only">Next</span>
+                  <svg className="w-2.5 h-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
                   </svg>
                 </p>
