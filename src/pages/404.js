4a35ee11b23c12a68610ee98754a7ddc67393586
zahra <zahra20hb@gmail.com>
2024-01-30 14:38:03 +0330

    responsive design for 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,34 +6,11 @@ export default function Custom404() {
     const router = useRouter()
 
     return (
-        <section class="bg-white dark:bg-gray-900 ">
-            <div class="container min-h-screen px-6 py-12 mx-auto lg:flex lg:items-center lg:gap-12">
-                <div class="wf-ull lg:w-1/2">
-                    <p class="text-sm font-medium text-blue-500 dark:text-blue-400">خطای 404</p>
-                    <h1 class="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">صفحه مورد نظر پیدا نشد. </h1>
-                    <p class="mt-4 text-gray-500 dark:text-gray-400">صفحه ای که شما دنبال آن هستید، پیدا نشد. لینک های زیر کمک کننده هستند:</p>
+        <section class="bg-white dark:bg-gray-900 h-[90vh]">
+            <div class="container min-h-screen px-6 py-12 mx-auto lg:flex items-center lg:gap-12">
+                <div class="relative sm:w-full flex justify-center items-center lg:w-1/2">
 
-                    <div class="flex items-center mt-6 gap-x-3">
-                        <button class="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 rtl:rotate-180">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
-                            </svg>
-
-
-                            <span>
-                                <Link onClick={() => router.back()} href={'/'}>برگشت</Link>
-                            </span>
-                        </button>
-
-                        <Link href={'/'} class="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
-                            خانه
-                        </Link>
-                    </div>
-                </div>
-
-                <div class="relative w-full mt-12 lg:w-1/2 lg:mt-0">
-
-                    <svg width="514" height="164" viewBox="0 0 514 164" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <svg width="400" height="164" viewBox="0 0 514 164" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <circle cx="101" cy="22" r="20" stroke="#667085" stroke-width="2" />
                         <circle cx="101" cy="142" r="20" stroke="#667085" stroke-width="2" />
                         <circle cx="21" cy="102" r="20" stroke="#667085" stroke-width="2" />
@@ -59,6 +36,30 @@ export default function Custom404() {
                     </svg>
 
                 </div>
+                <div class="w-full lg:w-1/2">
+                    <p class="text-sm font-medium text-blue-500 dark:text-blue-400">خطای 404</p>
+                    <h1 class="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">صفحه مورد نظر پیدا نشد. </h1>
+                    <p class="mt-4 text-gray-500 dark:text-gray-400">صفحه ای که شما دنبال آن هستید، پیدا نشد. لینک های زیر کمک کننده هستند:</p>
+
+                    <div class="flex items-center mt-6 gap-x-3">
+                        <button class="flex items-center justify-center px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 rtl:rotate-180">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+                            </svg>
+
+
+                            <span>
+                                <Link onClick={() => router.back()} href={'/'}>برگشت</Link>
+                            </span>
+                        </button>
+
+                        <Link href={'/'} class=" px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
+                            خانه
+                        </Link>
+                    </div>
+                </div>
+
+
             </div>
         </section>
     )
