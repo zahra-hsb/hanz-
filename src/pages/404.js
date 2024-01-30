c40e8d2e651c08fd02e2d5e2724b7fb0ca3333e0
zahra <zahra20hb@gmail.com>
2024-01-30 13:50:05 +0330

    404 page created

---
+++ src/pages/404.js
@@ -0,0 +1,7 @@
+export default function Custom404() {
+    return (
+        <div className="w-full h-[100vh] flex items-center justify-center">
+            <h4 className="text-red-500">404 - Page not found</h4>
+        </div>
+    )
+}
