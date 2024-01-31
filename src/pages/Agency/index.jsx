bc3f7a7b23fb95f42cadc8a41478afc94b685cef
zahra <zahra20hb@gmail.com>
2024-01-31 14:33:14 +0330

    agency page created

--- src/pages/Agency/index.jsx
+++ src/pages/Agency/index.jsx
@@ -3,7 +3,7 @@ import Main from "@/components/globalComponents/Main"
 const Agency = () => {
     return (
         <>
-            <Main title={'نمایندگی'} bg={'bg-agencySection'} />
+            <Main title={'نمایندگی'} bg={'bg-agencySection'} height={'h-[50vh]'} display={'hidden'}/>
         </>
     )
 }
