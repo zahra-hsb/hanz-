0e1c051983220e942d8a0d519dbb28248d5ca9f3
zahra <zahra20hb@gmail.com>
2024-01-31 12:09:11 +0330

    dark codes deleted

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,9 +6,9 @@ export default function Custom404() {
     const router = useRouter()
 
     return (
-        <section class="bg-white dark:bg-gray-900 h-[90vh]">
-            <div class="container min-h-screen px-6 py-12 mx-auto lg:flex items-center lg:gap-12">
-                <div class="relative sm:w-full flex justify-center items-center lg:w-1/2">
+        <section className="bg-white h-[90vh]">
+            <div className="container min-h-screen px-6 py-12 mx-auto lg:flex items-center lg:gap-12">
+                <div className="relative sm:w-full flex justify-center items-center lg:w-1/2">
 
                     <svg width="400" height="164" viewBox="0 0 514 164" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <circle cx="101" cy="22" r="20" stroke="#00ae8e" stroke-width="2" />
@@ -36,14 +36,14 @@ export default function Custom404() {
                     </svg>
 
                 </div>
-                <div class="w-full lg:w-1/2">
-                    <p class="text-sm font-medium text-green dark:text-blue-400">خطای 404</p>
-                    <h1 class="mt-3 text-2xl text-gray-500 dark:text-white md:text-3xl">صفحه مورد نظر پیدا نشد. </h1>
-                    <p class="mt-4 text-gray-500 dark:text-gray-400">صفحه ای که شما دنبال آن هستید، پیدا نشد. لینک های زیر کمک کننده هستند:</p>
+                <div className="w-full lg:w-1/2">
+                    <p className="text-sm font-medium text-green ">خطای 404</p>
+                    <h1 className="mt-3 text-2xl text-gray-500 md:text-3xl">صفحه مورد نظر پیدا نشد. </h1>
+                    <p className="mt-4 text-gray-500">صفحه ای که شما دنبال آن هستید، پیدا نشد. لینک های زیر کمک کننده هستند:</p>
 
-                    <div class="flex items-center mt-6 gap-x-3">
-                        <button class="flex items-center justify-center px-5 py-2 text-sm text-gray-700 transition-all duration-300 bg-white border rounded-lg gap-x-2 w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 rtl:rotate-180">
+                    <div className="flex items-center mt-6 gap-x-3">
+                        <button className="flex items-center justify-center px-5 py-2 text-sm text-gray-700 transition-all duration-300 bg-white border rounded-lg gap-x-2 w-auto hover:bg-gray-100 ">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5 rtl:rotate-180">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
                             </svg>
 
@@ -53,7 +53,7 @@ export default function Custom404() {
                             </span>
                         </button>
 
-                        <Link href={'/'} class=" px-5 py-2 text-sm tracking-wide text-white transition-all duration-300 bg-green rounded-lg shrink-0 sm:w-auto hover:opacity-[80%] dark:hover:bg-blue-500 dark:bg-blue-600">
+                        <Link href={'/'} className=" px-5 py-2 text-sm tracking-wide text-white transition-all duration-300 bg-green rounded-lg shrink-0 sm:w-auto hover:opacity-[80%]">
                             خانه
                         </Link>
                     </div>
