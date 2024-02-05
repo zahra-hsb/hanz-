2fac3853bc480f2086e7f3e204e4005e770f52eb
mohamad sd <mohamadsedighbakhshaish@gmail.com>
2024-02-05 12:46:07 +0330

    edit CatalogueHinge in Builing page

--- src/components/prductsComponents/BuilingComponent/CatalogueHinge/index.jsx
+++ src/components/prductsComponents/BuilingComponent/CatalogueHinge/index.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import profile1Image from "../../../../../public/image/catalogue3d/profile-1.jpg"
-import profile2Image from "../../../../../public/image/catalogue3d/profile-2.jpg"
-import profile3Image from "../../../../../public/image/catalogue3d/profile-3.jpg"
+import noneProfileImage from "../../../../../public/image/catalogue3d/none-profile.jpg"
+import profile115TImage from "../../../../../public/image/catalogue3d/115-t.jpg"
+import profile60NImage from "../../../../../public/image/catalogue3d/60n.jpg"
+import profile77DImage from "../../../../../public/image/catalogue3d/77d.jpg"
 import Pagination from "../../../pagination/index"
 import CatalogueCardComponent from "../catalogueComponent/CatalogueCardComponent";
 
@@ -11,38 +12,43 @@ const CatalogueHinge = () => {
     const catalogueDataHinge = [
         {
             title: <div>سیستم <span>115T</span></div>,
-            image: profile1Image,
-            link: 'https://drive.google.com/file/d/1RFnFfzeHH-TKMs4emLdflmKEC7wY4LJ8/view?usp=sharing'
+            image: profile115TImage,
+            link: '#'
         },
         {
             title: <div>سیستم <span>60N</span></div>,
-            image: profile2Image,
-            link: 'https://drive.google.com/file/d/1RFnFfzeHH-TKMs4emLdflmKEC7wY4LJ8/view?usp=sharing'
+            image: profile60NImage,
+            link: '#'
         },
         {
             title: <div>سیستم <span>68LUX</span></div>,
-            image: profile3Image,
-            link: 'https://drive.google.com/file/d/1RFnFfzeHH-TKMs4emLdflmKEC7wY4LJ8/view?usp=sharing'
+            image: noneProfileImage,
+            link: '#'
         },
         {
             title: <div>سیستم <span>68Decorative</span></div>,
-            image: profile1Image,
-            link: 'https://drive.google.com/file/d/1RFnFfzeHH-TKMs4emLdflmKEC7wY4LJ8/view?usp=sharing'
+            image: noneProfileImage,
+            link: '#'
         },
         {
             title: <div>سیستم <span>68ECO</span></div>,
-            image: profile2Image,
-            link: 'https://drive.google.com/file/d/1RFnFfzeHH-TKMs4emLdflmKEC7wY4LJ8/view?usp=sharing'
+            image: noneProfileImage,
+            link: '#'
         },
         {
             title: <div>سیستم <span>64LUX</span></div>,
-            image: profile3Image,
-            link: 'https://drive.google.com/file/d/1RFnFfzeHH-TKMs4emLdflmKEC7wY4LJ8/view?usp=sharing'
+            image: noneProfileImage,
+            link: '#'
         },
         {
             title: <div>سیستم <span>64ECO</span></div>,
-            image: profile1Image,
-            link: 'https://drive.google.com/file/d/1RFnFfzeHH-TKMs4emLdflmKEC7wY4LJ8/view?usp=sharing'
+            image: noneProfileImage,
+            link: '#'
+        },
+        {
+            title: <div>سیستم <span>77Decoral</span></div>,
+            image: profile77DImage,
+            link: '#'
         },
     ];
 
@@ -81,7 +87,7 @@ const CatalogueHinge = () => {
                             </div>
                         </div>
                     ))} */}
-                    <CatalogueCardComponent receivedData={receivedDataHinge}/>
+                    <CatalogueCardComponent receivedData={receivedDataHinge} />
                 </div>
             </div>
 
