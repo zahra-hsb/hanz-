97c7031b6eed5c221c3fd2eec26e768a02015633
mohamad sd <mohamadsedighbakhshaish@gmail.com>
2024-02-05 16:29:07 +0330

    edit WhyHanzHome component

--- src/components/homeComponents/WhyHanzHome/index.jsx
+++ src/components/homeComponents/WhyHanzHome/index.jsx
@@ -3,6 +3,7 @@ import hanzProfile1 from '../../../../public/image/hanz-profile1.webp'
 import hanzProfile2 from '../../../../public/image/hanz-profile2.webp'
 import hanzProfile3 from '../../../../public/image/hanz-profile3.webp'
 import Button from "@/components/globalComponents/Main/button";
+import WhyHanzCard from '@/components/WhyHanzCard'
 // import cashOnDelivery from '../../../../public/icons/cash-on-delivery.svg'
 
 
@@ -19,36 +20,40 @@ const WhyHanzHome = () => {
             <div className="flex w-full lg:w-[90%] lg:m-auto flex-col lg:flex-row items-center justify-between">
 
                 <div className="w-full">
-                    <div className="mb-10 flex justify-between items-center text-xl text-gray-600 w-[90%] sm:w-[100%] m-auto">
-                        {/* <div>
-                            <h2>چرا هانز</h2>
-                        </div> */}
-
-                        <div className="flex w-full flex-col lg:flex-row items-center justify-between">
+                    <div className="bg-gray-100 rounded-lg mx-2 sm:mx-0">
+                        <div className="px-6 mb-10 flex justify-between items-center text-xl text-gray-600 w-[90%] sm:w-[100%] m-auto">
                             {/* <div>
-                        <Image src={picture} alt="hanz" />
-                    </div> */}
-                            <div className="pt-8 w-full">
-                                <div className="flex justify-between items-center">
-                                    <h4 className="text-2xl opacity-[76%]">
-                                        چرا گروه
-                                        <span className="text-5xl text-green opacity-1"> هانز</span>
-                                    </h4>
-                                    <div>
-                                        <Button title={'درباره ما'} url={'/about'} border={'border-2 border-green'} hover={'hover:bg-green hover:text-white'} />
+                                <h2>چرا هانز</h2>
+                            </div> */}
+                            <div className="flex w-full flex-col lg:flex-row items-center justify-between">
+                                {/* <div>
+                                    <Image src={picture} alt="hanz" />
+                                </div> */}
+                                <div className="pt-8 w-full">
+                                    <div className="flex justify-between items-center">
+                                        <h4 className="text-2xl opacity-[85%]">
+                                            چرا گروه
+                                            <span className="text-5xl text-green opacity-1"> هانز</span>
+                                        </h4>
+                                        <div>
+                                            <Button title={'درباره ما'} url={'/about'} border={'border-2 border-green'} hover={'hover:bg-green hover:text-white'} />
+                                        </div>
+                                    </div>
+                                    <div className="lg:w-[60%]">
+                                        <p className="pt-5 lg:w-5/6 text-justify opacity-[75%] text-lg">
+                                            شرکت هانز یکی از معتبرترین شرکت های طراحی و تولیدکننده سیستم های نوین آلومینیومی در کشور میباشد که با عقبه دیرینه خود اقدام به تولید کلی مقاطع آلومینیومی نموده است.
+                                        </p>
                                     </div>
                                 </div>
-                                <div className="lg:w-[60%]">
-                                    <p className="pt-5 lg:w-5/6 text-justify opacity-50 text-lg">
-                                        شرکت هانز یکی از معتبرترین شرکت های طراحی و تولیدکننده سیستم های نوین آلومینیومی در کشور میباشد که با عقبه دیرینه خود اقدام به تولید کلی مقاطع آلومینیومی نموده است.
-                                    </p>
-                                </div>
                             </div>
+
                         </div>
 
-                    </div>
+                        <div>
+                            <WhyHanzCard />
+                        </div>
 
-                    <div id="WhyHanzHome-carousel" className="w-full">
+                        {/* <div id="WhyHanzHome-carousel" className="w-full">
                         <Slider
                             url='/about'
                             slidesPerView={3}
@@ -56,9 +61,9 @@ const WhyHanzHome = () => {
                             textSlider={true}
                             textColor={'text-gray-600'}
                         />
+                    </div> */}
                     </div>
                 </div>
-
             </div>
         </section>
     );
