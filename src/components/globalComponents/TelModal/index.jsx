7b6efcece81eec06965328f1a25a4c3c75e066d8
zahra <zahra20hb@gmail.com>
2024-02-12 12:33:45 +0330

    tel modal created

---
+++ src/components/globalComponents/TelModal/index.jsx
@@ -0,0 +1,13 @@
+import TelInfo from "@/components/contactUsComponents/TelInfo"
+
+const TelModal = () => {
+    return (
+        <>
+            <main className="w-[160px] absolute bottom-10 -left-16 bg-white p-5 shadow-lg flex flex-col gap-3">
+                <TelInfo />
+            </main>
+        </>
+    )
+}
+
+export default TelModal
