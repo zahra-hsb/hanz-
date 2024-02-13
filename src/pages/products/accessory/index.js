f5bcd35c6ebd06fc2ba71c4dbbf1fcbf486b43ab
mohamad sd <mohamadsedighbakhshaish@gmail.com>
2024-02-13 12:12:07 +0330

    edit Accessory page

--- src/pages/products/accessory/index.js
+++ src/pages/products/accessory/index.js
@@ -9,6 +9,22 @@ export default function Accessory() {
       <Main title={'اکسسوری'} bg={'bg-profileSection'} height={'h-[390px]'} display={'hidden'} />
       <div className="w-[100%] sm:w-[90%] m-auto">
 
+        <div>
+          <div className="mt-20 mb-10 text-gray-600">
+            <div>
+              <h4 className="text-2xl opacity-[76%]">
+                محصولات
+                <span className="text-4xl text-green opacity-1"> صنعتی</span>
+              </h4>
+            </div>
+          </div>
+          <div>
+            <p className="lg:w-5/6 text-justify text-gray-600 text-lg">
+              .گروه صنعتی هانز محصولات آلومینیومی متنوعی در صنایع الکترونیک، پزشکی، لوازم خانگی و ابزارهای صنعتی با بهره مندی از خطوط متعدد تولیدی و قطعه سازی، آماده ی ارائه ی بهترین خدمات برای پاسخ به نیاز های متنوع مشتریان صنعتی است.
+            </p>
+          </div>
+        </div>
+
         <Profile />
         <ProfileProperties />
         <Cratinwall />
