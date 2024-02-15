dcba446e100f80ebaeca0c42a76e366780227dd9
zahra <zahra20hb@gmail.com>
2024-02-15 10:51:49 +0330

    assist page main section created

--- src/pages/assist/index.js
+++ src/pages/assist/index.js
@@ -1,7 +1,9 @@
+import Main from "@/components/globalComponents/Main"
+
 const Assist = () => {
     return (
         <>
-            
+            <Main title={'دعوت به همکاری'} bg={'bg-assistSection'} height={'h-[400px]'} display={'hidden'}/>
         </>
     )
 }
