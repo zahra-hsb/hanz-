69efaad5b4b16150206758211c70d57ee7770c58
zahra <zahra20hb@gmail.com>
2024-02-17 14:13:36 +0330

    icons for follow us styled

--- src/components/globalComponents/FollowUsSection/index.jsx
+++ src/components/globalComponents/FollowUsSection/index.jsx
@@ -1,11 +1,13 @@
 import { SlUserFollow } from "react-icons/sl";
+import { IoIosArrowDown } from "react-icons/io";
 
 
 const FollowUsSection = () => {
     return (
         <>
-            <main className="flex justify-end p-8">
-                <SlUserFollow />
+            <main className="flex justify-end gap-2 items-end px-8 text-gray-700 ">
+                <IoIosArrowDown className="cursor-pointer hover:text-gray-400" size={15}/>
+                <SlUserFollow className="cursor-pointer hover:text-gray-400" size={20} />
             </main>
         </>
     )
