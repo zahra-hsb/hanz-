303b6e26bc4caebfdcab6ca64504ca4e54046fab
zahra <zahra20hb@gmail.com>
2024-02-18 15:03:29 +0330

    handle change language and some problem fixed

--- src/components/layout/Sidebar/utils-sidebar/LangSection.jsx
+++ src/components/layout/Sidebar/utils-sidebar/LangSection.jsx
@@ -1,7 +1,21 @@
-const LangSection = () => {
+import Image from "next/image"
+
+const LangSection = ({ languages = [], changeLang }) => {
+
+
+
     return (
         <>
-
+            <div className="absolute -top-40 left-0 w-full bg-white flex flex-col gap-5 items-center px-5 py-6 shadow-lg border-t">
+                {languages.map(item => (
+                    <>
+                        <div type="radio" className="flex gap-3 items-center justify-start w-full" >
+                            <Image src={item.src} id="flag" alt="farsi" className="" />
+                            <p onClick={changeLang}  className="hover:text-gray-500 cursor-pointer">{item.title}</p>
+                        </div>
+                    </>
+                ))}
+            </div>
         </>
     )
 }
--- src/components/layout/Sidebar/utils-sidebar/Language.jsx
+++ src/components/layout/Sidebar/utils-sidebar/Language.jsx
@@ -1,16 +1,43 @@
 import Image from "next/image"
 import iranFlag from '../../../../../public/icons/Flag_of_Iran.svg.webp'
 import { IoIosArrowDown } from "react-icons/io";
+import { useState } from "react";
+import LangSection from "./LangSection";
+import arabic from '../../../../../public/icons/32px-Arabic-Language-Flag.svg.png'
+import english from '../../../../../public/icons/Flag_of_the_United_Kingdom_(3-5).svg.webp'
+
+
 
 const Language = () => {
+    const languages = [
+        { title: 'فارسی', src: iranFlag },
+        { title: 'العربی', src: arabic },
+        { title: 'English', src: english }
+    ]
+    const [isOpen, setOpen] = useState(false)
+    const [currentLang, setCurrentLang] = useState(languages[0])
+
+    function changeLang(e) {
+        const foundCurrentLang = languages.find(item =>
+            item.title === e.target.innerText 
+        )
+        setCurrentLang(foundCurrentLang)
+
+    }
+
+    function showLang() {
+        setOpen(!isOpen)
+    }
     return (
         <>
-            <div className="w-full bg-white flex justify-between items-center px-5 py-5 border-t">
+            <div className="relative w-full bg-white flex justify-between items-center px-5 py-5 border-t">
                 <div className="flex gap-3">
-                    <Image src={iranFlag} alt="farsi" className="cursor-pointer"/>
-                    <p className="hover:text-gray-500 cursor-pointer">فارسی</p>
+                    <Image onClick={showLang} width={35} height={20} src={currentLang.src.src} alt="farsi" className="cursor-pointer" />
+                    <p onClick={showLang} className="hover:text-gray-500 cursor-pointer">{currentLang.title}</p>
                 </div>
-                <IoIosArrowDown className="cursor-pointer hover:text-gray-500 rotate-180" />
+                <IoIosArrowDown onClick={showLang} className={` ${isOpen ? 'rotate-180' : 'rotate-0'} cursor-pointer hover:text-gray-500 duration-300`} />
+                {isOpen && <LangSection languages={languages} changeLang={changeLang} />}
+
             </div>
         </>
     )
